8cbb1831548113d8d045445bbced1271d0907876
Ange Albertini <angea@users.noreply.github.com>
2010-01-10 19:18:32 +0000

    added Opcodes32 wrapper corrected path

--- misc/MakePE/examples/helloworld.asm
+++ misc/MakePE/examples/helloworld.asm
@@ -1,7 +1,7 @@
 ;simple helloworld example
 ;compile with: makepe.py helloworld.asm
 
-%include 'onesec.hdr'
+%include '../onesec.hdr'
 
 MB_ICONINFORMATION equ 040h
 EntryPoint:
---
+++ misc/MakePE/examples/opcodes32pe.asm
@@ -0,0 +1,13 @@
+;PE wrap around opcodes32
+;compile with makePE
+IMPORT_DESCRIPTOR equ IMAGEBASE
+
+%include '../onesec.hdr'
+
+EntryPoint:
+%include '../../opcodes32.asm'
+
+SECTION0SIZE equ $ - Section0Start
+SIZEOFIMAGE equ $ - IMAGEBASE
+
+;Ange Albertini 2009-2010
