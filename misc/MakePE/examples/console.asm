5ac0bdbb9ddb939df9594d5a92aff44e0273f6b9
Ange Albertini <angea@users.noreply.github.com>
2010-01-19 19:18:41 +0000

    added subsystems files

---
+++ misc/MakePE/examples/console.asm
@@ -0,0 +1,58 @@
+%include '../standard_hdr.asm'
+
+;%DEFINE SUBSYSTEM
+SUBSYSTEM equ IMAGE_SUBSYSTEM_WINDOWS_CUI
+
+STD_OUTPUT_HANDLE equ -11
+
+EntryPoint:
+    push STD_OUTPUT_HANDLE  ; DWORD nStdHandle
+    call GetStdHandle
+    mov [hConsoleOutput], eax
+
+;    call GetCommandLineA
+;    mov ebx, eax
+;    mov edi, eax
+;
+;    mov al, 0
+;    mov ecx, 0xffffffff
+;    cld
+;    repnz scasb
+;    neg ecx
+;    sub ecx, 2
+
+;    ; standard GUI calls still work
+;    push MB_ICONINFORMATION ; UINT uType
+;    push tada               ; LPCTSTR lpCaption
+;    push helloworld         ; LPCTSTR lpText
+;    push 0                  ; HWND hWnd
+;    call MessageBoxA
+;IMPORT user32.dll!MessageBoxA
+
+    push 0                          ; LPVOID lpReserved
+    push lpNumbersOfCharsWritten    ; LPWORD lpNumbersOfCharsWritten
+    push HELLOWORLD_LEN             ; DWORD nNumbersOfCharsToWrite
+    push helloworld                 ; VOID *lpBuffer
+    push dword [hConsoleOutput]     ; HANDLE hConsoleOutput
+    call WriteConsoleA
+
+    push 0                  ; UINT uExitCode
+    call ExitProcess
+    retn
+
+lpNumbersOfCharsWritten dd 0
+hConsoleOutput dd 0
+;tada db "Tada!", 0
+helloworld db "Hello World!"
+    HELLOWORLD_LEN equ $ - helloworld
+db 0
+
+;%IMPORT kernel32.dll!GetStdHandle
+;IMPORT kernel32.dll!GetCommandLineA
+;%IMPORT kernel32.dll!WriteConsoleA
+;%IMPORT kernel32.dll!ExitProcess
+;%IMPORTS
+
+%include '../standard_ftr.asm'
+
+; Ange Albertini 2009-2010
---
+++ misc/MakePE/examples/driver.asm
@@ -0,0 +1,45 @@
+; minimal hello world driver
+
+%include '..\standard_hdr.asm'
+
+;%DEFINE IMAGEBASE SUBSYSTEM CHARACTERISTICS DIRECTORY_ENTRY_BASERELOC DIRECTORY_ENTRY_BASERELOC_SIZE
+
+IMAGEBASE EQU 10000H
+SUBSYSTEM EQU IMAGE_SUBSYSTEM_NATIVE
+CHARACTERISTICS EQU IMAGE_FILE_EXECUTABLE_IMAGE | IMAGE_FILE_32BIT_MACHINE
+
+STATUS_DEVICE_CONFIGURATION_ERROR equ 0C0000182h
+
+EntryPoint:
+reloc1_1:
+    push helloworld     ; PCHAR  Format
+    call DbgPrint
+    add esp, 4
+
+    mov eax, STATUS_DEVICE_CONFIGURATION_ERROR
+    retn 8
+
+helloworld db "Hello World!", 0
+
+reloc2_2:
+;%IMPORT ntoskrnl.exe!DbgPrint
+;%IMPORTS
+
+;relocations start
+DIRECTORY_ENTRY_BASERELOC:
+base_reloc:
+; relocation block start
+    .VirtualAddress dd Section0Start - IMAGEBASE
+    .SizeOfBlock dd base_reloc_size_of_block
+    dw (IMAGE_REL_BASED_HIGHLOW << 12) | (reloc1_1 + 1 - Section0Start)
+    dw (IMAGE_REL_BASED_HIGHLOW << 12) | (reloc2_2 + 2 - Section0Start)
+    base_reloc_size_of_block equ $ - base_reloc
+;relocation block end
+
+;relocations end
+
+DIRECTORY_ENTRY_BASERELOC_SIZE equ $ - DIRECTORY_ENTRY_BASERELOC
+
+%include '..\standard_ftr.asm'
+
+; Ange Albertini 2009-2010
