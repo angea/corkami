9f3de41b95a1a377593c595e88264f9d47a111ad
Ange Albertini <angea@users.noreply.github.com>
2010-02-13 00:00:05 +0000

    cosmetic changes

--- misc/MakePE/examples/sys2exe.py
+++ misc/MakePE/examples/sys2exe.py
@@ -1,9 +1,10 @@
 # turns a driver into a GUI version + GUI with imports removed
+
 import pefile, sys
 fn = sys.argv[1]
 p = pefile.PE(fn)
 
-p.OPTIONAL_HEADER.Subsystem = 2
+p.OPTIONAL_HEADER.Subsystem = 2 # pefile.SUBSYSTEM_TYPE['IMAGE_SUBSYSTEM_WINDOWS_GUI']
 p.write(fn.replace(".sys",".exe"))
 
 p.OPTIONAL_HEADER.DATA_DIRECTORY[1].VirtualAddress = 0
