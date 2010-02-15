6c41dca0618e61b8ae723ce1ea7b41d1cfe88c03
Ange Albertini <angea@users.noreply.github.com>
2010-02-15 23:17:30 +0000

    moved code to headers, EP now aligned and smaller

--- misc/MakePE/examples/tls_zeroes.asm
+++ misc/MakePE/examples/tls_zeroes.asm
@@ -1,7 +1,68 @@
 ; empty entry point file, looks buggy as much as possible but TLS is setting things correctly
-; would look better with split sections or other code in header
 
-%include '..\standard_hdr.asm'
+;;;;;;;;;;;;;;;;;;;;;;;;; this is onesec.hdr
+%include '..\consts.asm'
+
+FILEALIGN equ 4h
+SECTIONALIGN equ FILEALIGN  ; different alignements are not supported by MakePE
+org IMAGEBASE
+
+istruc IMAGE_DOS_HEADER
+    at IMAGE_DOS_HEADER.e_magic, db 'MZ'
+    at IMAGE_DOS_HEADER.e_lfanew, dd nt_header - IMAGEBASE
+iend
+
+nt_header:
+istruc IMAGE_NT_HEADERS
+    at IMAGE_NT_HEADERS.Signature, db 'PE',0,0
+iend
+istruc IMAGE_FILE_HEADER
+    at IMAGE_FILE_HEADER.Machine,               dw IMAGE_FILE_MACHINE_I386
+    at IMAGE_FILE_HEADER.NumberOfSections,      dw NUMBEROFSECTIONS
+    at IMAGE_FILE_HEADER.SizeOfOptionalHeader,  dw SIZEOFOPTIONALHEADER
+    at IMAGE_FILE_HEADER.Characteristics,       dw CHARACTERISTICS
+iend
+
+OptionalHeader:
+istruc IMAGE_OPTIONAL_HEADER32
+    at IMAGE_OPTIONAL_HEADER32.Magic                    , dw IMAGE_NT_OPTIONAL_HDR32_MAGIC
+    at IMAGE_OPTIONAL_HEADER32.AddressOfEntryPoint      , dd EntryPoint - IMAGEBASE
+    at IMAGE_OPTIONAL_HEADER32.ImageBase                , dd IMAGEBASE
+    at IMAGE_OPTIONAL_HEADER32.SectionAlignment         , dd SECTIONALIGN
+    at IMAGE_OPTIONAL_HEADER32.FileAlignment            , dd FILEALIGN
+    at IMAGE_OPTIONAL_HEADER32.MajorSubsystemVersion    , dw 4
+    at IMAGE_OPTIONAL_HEADER32.SizeOfImage              , dd SIZEOFIMAGE
+    at IMAGE_OPTIONAL_HEADER32.SizeOfHeaders            , dd SIZEOFHEADERS  ; can be 0 in some circumstances
+    at IMAGE_OPTIONAL_HEADER32.Subsystem                , dw IMAGE_SUBSYSTEM_WINDOWS_GUI
+    at IMAGE_OPTIONAL_HEADER32.NumberOfRvaAndSizes      , dd NUMBEROFRVAANDSIZES
+iend
+
+DataDirectory:
+istruc IMAGE_DATA_DIRECTORY_16
+    at IMAGE_DATA_DIRECTORY_16.ExportsVA,   dd Exports_Directory - IMAGEBASE
+    at IMAGE_DATA_DIRECTORY_16.ImportsVA,   dd IMPORT_DESCRIPTOR - IMAGEBASE
+    at IMAGE_DATA_DIRECTORY_16.ResourceVA,  dd Directory_Entry_Resource - IMAGEBASE
+    at IMAGE_DATA_DIRECTORY_16.FixupsVA,    dd Directory_Entry_Basereloc - IMAGEBASE
+    at IMAGE_DATA_DIRECTORY_16.FixupsSize,  dd DIRECTORY_ENTRY_BASERELOC_SIZE
+    at IMAGE_DATA_DIRECTORY_16.TLSVA,       dd Image_Tls_Directory32 - IMAGEBASE
+iend
+
+NUMBEROFRVAANDSIZES equ ($ - DataDirectory) / IMAGE_DATA_DIRECTORY_size
+
+SIZEOFOPTIONALHEADER equ $ - OptionalHeader
+
+SectionHeader:
+istruc IMAGE_SECTION_HEADER
+    at IMAGE_SECTION_HEADER.VirtualSize, dd SECTION0SIZE
+    at IMAGE_SECTION_HEADER.VirtualAddress, dd Section0Start - IMAGEBASE
+    at IMAGE_SECTION_HEADER.SizeOfRawData, dd SECTION0SIZE
+    at IMAGE_SECTION_HEADER.PointerToRawData, dd Section0Start - IMAGEBASE
+iend
+NUMBEROFSECTIONS equ ($ - SectionHeader) / IMAGE_SECTION_HEADER_size
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; until here
+
+bits 32
 
 stub:
     push MB_ICONINFORMATION     ; UINT uType
@@ -11,6 +72,9 @@ stub:
     call MessageBoxA
     push 0                      ; UINT uExitCode
     call ExitProcess
+
+;%IMPORT user32.dll!MessageBoxA
+;%IMPORT kernel32.dll!ExitProcess
 
 TLS:
     mov eax, [fs:0]
@@ -27,11 +91,6 @@ TLS:
 aEntryPoint db "Entry Point", 0
 helloworld db "Hello World!", 0
 
-;%IMPORT user32.dll!MessageBoxA
-;%IMPORT kernel32.dll!ExitProcess
-
-;%IMPORTS
-
 Image_Tls_Directory32:
     StartAddressOfRawData dd Characteristics ; VA, should point to something null
     EndAddressOfRawData   dd Characteristics ; VA, should point to something null
@@ -44,8 +103,18 @@ Callbacks:
     dd TLS
     dd 0
 
-EntryPoint:
-    times 10h add [eax], al
-%include '..\standard_ftr.asm'
+align FILEALIGN, db 0
+SIZEOFHEADERS equ $ - IMAGEBASE
+Section0Start:
 
-; Ange Albertini, Creative Commons BY, 2009-2010
+;Can't be in headers
+;%IMPORTS
+align 100h, db 0
+EntryPoint:
+    times 5h add [eax], al
+    db 0
+
+SECTION0SIZE equ $ - Section0Start
+SIZEOFIMAGE equ $ - IMAGEBASE
+
+; Ange Albertini, Creative Commons BY, 2010
