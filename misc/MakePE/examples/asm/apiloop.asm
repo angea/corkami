b608dbf3f3aef1df0af2564810f5c9ceb7e018fe
Ange Albertini <angea@users.noreply.github.com>
2010-03-02 22:17:56 +0000

    added file that use an api return value as increment

---
+++ misc/MakePE/examples/asm/apiloop.asm
@@ -0,0 +1,59 @@
+; some api may return after each call a value that has been incremented
+; in a predictable way, so it's usable as an anti-emulator
+
+%include '..\..\onesec.hdr'
+
+%macro myapi 0
+    push 0
+    push 0
+    call GlobalAlloc
+%endmacro
+
+counter equ 10
+
+EntryPoint:
+    myapi
+    mov [base], eax ; increment calculation
+
+    myapi
+    sub eax, [base]
+    mov [increment], eax
+
+    mov eax, [increment]
+    mov ecx, counter + 1
+    mul ecx
+    add eax, [base]
+
+    mov [limit], eax
+
+    mov ebx, 0
+
+_loop:
+    inc ebx
+
+    myapi
+    cmp [limit], eax
+    jnz _loop
+
+    cmp ebx, counter        ; let's confirm the amount of iterationwas correct
+    jnz bad
+
+    jmp good
+
+;%IMPORT kernel32.dll!GlobalAlloc
+
+base dd 0
+increment dd 0
+limit dd 0
+
+%include '../goodbad.inc'
+
+;%IMPORT user32.dll!MessageBoxA
+;%IMPORT kernel32.dll!ExitProcess
+
+;%IMPORTS
+
+SECTION0SIZE equ $ - Section0Start
+SIZEOFIMAGE equ $ - IMAGEBASE
+
+;Ange Albertini, Creative Commons BY, 2010
