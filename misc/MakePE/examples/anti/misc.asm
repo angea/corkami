d9d68335963e79d0684545f33b89f0a9c9c7d12d
Ange Albertini <angea@users.noreply.github.com>
2010-03-09 20:34:30 +0000

    added forceflag, pop ss, outputdbgstring, ntqueryinfo, checkremote

--- misc/MakePE/examples/anti/misc.asm
+++ misc/MakePE/examples/anti/misc.asm
@@ -4,13 +4,21 @@
 
 
 EntryPoint:
-    call deletefib
     call trig
+
     call beingdebugged
     call ntglobalflag
+    call forceflag
+    call deletefib
+
+    call popss
+    call outputdbg
+;    call ntqueryinfo
+;    call checkremote
 
     jmp good
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; DeleteFiber
 ; triggers a BREAKPOINT exception if debugger present, else just an ERROR INVALID PARAMETER
 ; (ForceFlag check)
@@ -28,6 +36,9 @@ deletefib:
 ;%IMPORT kernel32.dll!GetLastError
 ;%IMPORT kernel32.dll!DeleteFiber
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+
 trig:
     push 0
     call _CIasin
@@ -37,12 +48,18 @@ trig:
     retn
 ;%IMPORT msvcrt.dll!_CIasin
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+
 beingdebugged:
     getPEB eax
     movzx eax, byte [eax + PEB.BeingDebugged]
     test eax, eax
     jnz bad
     retn
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
 
 ntglobalflag:
     getPEB eax
@@ -51,6 +68,86 @@ ntglobalflag:
     test eax, eax
     jnz bad
     retn
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+forceflag:
+    mov eax, [fs:30h]
+    mov eax, [eax+18h] ;process heap
+    mov eax, [eax+10h] ;heap.forceflags flags
+    test eax, eax
+    jnz bad
+    retn
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+
+popss:              ; anti-stepping
+    push ss
+    pop ss
+    pushf           ; debugger can't step directly after pop ss
+    pop eax
+TF equ 0100h
+    and eax, TF
+    test eax, eax
+    jnz bad
+    retn
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+
+outputdbg:
+    push _ErrorCheck
+    call SetLastError
+    xor eax, eax
+    push error
+    call OutputDebugStringA
+    cmp eax, 1
+    jnz bad
+    call GetLastError
+    cmp eax, _ErrorCheck
+    jz bad
+    retn
+
+_ErrorCheck equ 123456h
+
+;%IMPORT kernel32.dll!SetLastError
+;IMPORT kernel32.dll!GetLastError
+;%IMPORT kernel32.dll!OutputDebugStringA
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+
+ntqueryinfo:
+    push 0
+    push 4
+    push isdebugged
+    push 7 ;ProcessDebugPort
+    push -1
+    call NtQueryInformationProcess
+    test eax, eax
+    jnz bad
+    cmp dword [isdebugged], 0
+    jnz bad
+
+;%IMPORT ntdll.dll!NtQueryInformationProcess
+
+isdebugged dd 0
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;
+
+checkremote:
+    push isdebugged
+    push -1
+    call CheckRemoteDebuggerPresent
+    test eax, eax
+    jnz bad
+    retn
+
+;%IMPORT kernel32.dll!CheckRemoteDebuggerPresent
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 %include '..\goodbad.inc'
 
