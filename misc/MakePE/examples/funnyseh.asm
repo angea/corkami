8f51806f0d350da0f540b932163c23828c71bcfc
Ange Albertini <angea@users.noreply.github.com>
2010-03-23 22:58:04 +0000

    added 2nd part

--- misc/MakePE/examples/funnyseh.asm
+++ misc/MakePE/examples/funnyseh.asm
@@ -1,6 +1,7 @@
 ; complex SEH with Context checks, and direct use of KiFastSystemCallRet
 
 %include '../onesec.hdr'
+EXCEPTION_ILLEGAL_INSTRUCTION equ 0c000001dh
 
 EntryPoint:
     sub esp, 38h
@@ -34,7 +35,7 @@ handler:
 
     lea edi, [ecx + edi * 2 + CONTEXT.regEip]
     mov edi, [edi]                          ; => edi - trigger
-    lea edi, [edi + good - trigger]         ; => edi = good
+    lea edi, [edi + part2 - trigger]         ; => edi = good
 
     push edi
     pop dword [ecx + CONTEXT.regEip]
@@ -53,6 +54,85 @@ handler:
     retn
 not_ret:
     jmp [7ffe0304h]                         ;hardcoded IMPORT ntdll.dll!KiFastSystemCallRet
+
+part2:
+clearSEH
+nop
+nop
+nop
+
+    sub eax, eax                            ; mov eax, 0
+    push dword [esp + eax]                  ; push ImageBase
+    push ebp
+    mov ebp, esp
+    pusha                                   ; initialize
+    test ecx, edx
+    call parse_seh_chain
+
+    mov eax, [esp + 4]
+    add eax, [7FFE0080h]
+    push esi
+    mov esi, eax
+    lodsd
+    cmp eax, EXCEPTION_ILLEGAL_INSTRUCTION
+    pop esi
+    jnz short not_in_handler
+
+    push ebp
+    mov ebp, esp
+    xor eax, eax
+    pusha
+    mov edx, eax
+    add eax, esp
+    mov dl, [7FFE02F8h + edx * 2]
+
+    call change_eax
+    mov ebp, eax
+    dec dword [ebp + 0ACh]
+
+    jnz short not_good
+    lea ecx, [edx + ebp - 33h]
+    add dword [ecx + 28h], 3
+    mov edx, [ecx + 28h]
+    add edx, good - trigger2
+    mov ebp, [ecx + 24h]
+    mov [ebp + 4], edx
+
+not_good:
+    popa
+    leave
+    retn                                    ; we should go to bad instead :)
+
+not_in_handler:
+    vmcall
+trigger2:
+    add esp, 8
+    mov [edx + 4], ebx
+    popa
+    leave
+    retn
+
+parse_seh_chain:
+    push dword [ebp + 4]
+    movzx ecx, byte [7FFE02F8h]
+    test ecx, 5
+    jz short loop_continue
+    shr ecx, cl
+    jz short loop_continue
+    mov edx, [fs:ecx * 4 - 48h]
+    mov edx, [edx]
+    mov ebx, [esp + 4]
+    xchg ebx, [edx + 4]
+    mov ecx, 720Ch
+    jmp dword [esp + 4]
+
+loop_continue:
+    jmp short parse_seh_chain
+
+change_eax:
+    lea esi, [eax + edx * 8 - 5E8h]
+    mov eax, [edx + esi - 0C3h]
+    retn
 
 %include 'goodbad.inc'
 
