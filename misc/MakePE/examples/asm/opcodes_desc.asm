5ba10fed9484df261e3ec29d8ebe5b352a8675e2
Ange Albertini <angea@users.noreply.github.com>
2010-03-30 18:35:10 +0000

    intermediate save - half done roughly

--- misc/MakePE/examples/asm/opcodes_desc.asm
+++ misc/MakePE/examples/asm/opcodes_desc.asm
@@ -5,7 +5,46 @@
     jnz bad
 %endmacro
 
+ValueEDI dd 0ED0h
+ValueESI dd 0E01h
+ValueEBP dd 0EEBE3141h     ; E B PI ;)
+ValueESP dd 0               ; unused
+ValueEBX dd 0EB1h
+ValueEDX dd 0ED1h
+ValueECX dd 0EC1h
+ValueEAX dd 0EA1h
+val dd ValueEDI
+
 EntryPoint:
+
+    xchg [val] , esp    ; makes a backup of ESP and temporarily change ESP to the start of the data
+    popad               ; read all the data into registers
+    mov esp, [val]       ; restore ESP and EAX
+
+    mov ax, 0304h ; 34
+    mov bx, 0307h ; 37
+    add ax, bx
+    aaa
+    expect ax, 0701h ;34 + 37 = 71
+
+    mov ax, 01234h ; 1234
+    mov bx, 0537h  ; 537
+    add ax, bx
+    daa
+    expect ax, 1771h ; 1234 + 537 = 1771
+
+    mov ax, 0701h
+    mov bx, 0304h
+    sub ax, bx
+    aas
+;    expect ax, 0307h wrong
+
+mov eax, 01771h         ; let's store a constant as BCD
+mov ebx, 01234h
+sub eax, ebx            ; we got the result wrong, so we'll use AAA
+das                     ; now eax has the right result
+expect eax, 537h
+
     mov eax, 3
     lea eax, [eax * 4 + 203Ah]
     expect eax, 203ah + 4 * 3
@@ -82,7 +121,60 @@ EntryPoint:
     sar al, 2
     expect al, -2
 
+    mov ax, 1111b
+    mov bx, 0100000000000000b
+    shld ax, bx, 3
+    expect ax, 1111010b
+
+    mov ax, 1101001b
+    mov bx, 101b
+    shrd ax, bx, 3
+    expect ax, 1010000000001101b
+
+    mov ax, 35
+    mov bl, 11
+    div bl          ; 35 = 3 * 11 + 2
+    expect al, 3    ; quo
+    expect ah, 2    ; rem
+
+    mov al, 11
+    mov bl, 3
+    mul bl
+    expect ax, 33
+
+    mov eax, 11
+    imul eax, eax, 3
+    expect eax, 33
+
+    push ds
+
+    mov ebx, addseg
+    lds eax, [ebx]
+    expect eax , 12345678h
+    push ds
+    pop eax
+    expect eax, 0
+
+    pop ds
+
+    stc
+    setc al
+    expect al, 1
+
+    clc
+    mov eax, 0
+    mov ebx, 3
+    cmovc eax, ebx
+    expect eax, 0
+
     jmp good
+
+align 4, db 0
+addseg:
+    dd 12345678h
+    dw 00h         ; standard value for DS
+
+align 4, db 0
 
 %include '..\goodbad.inc'
 
