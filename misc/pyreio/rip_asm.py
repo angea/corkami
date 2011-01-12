50c2868bd81aa6ef76e44ef0a51c40de5b561f99
Ange Albertini <angea@users.noreply.github.com>
2011-01-12 21:00:55 +0000

    utils.py: added generic disassembly function, entrypoint code getter, wildcarded string maker rip_asm: now using utils.py, and opening/re-opening temporary files for user feedback

--- misc/pyreio/rip_asm.py
+++ misc/pyreio/rip_asm.py
@@ -2,38 +2,45 @@
 # BSD Licence, Ange Albertini 2011
 
 from pyreio import *
-from pydasm import get_instruction, get_instruction_string, MODE_32, FORMAT_INTEL
-import os
 
 sel = getselection()
-offset = 0
-r = ["%i" % len(sel)]
 
-#TODO: get the source offset
-while offset < len(sel):
-    instruction = get_instruction(sel[offset:], MODE_32)
+if sel is None:
+    msg("Empty Selection ! aborting...")
+else:
+    from utils import get_disassembly, templatize, seq_to_snippets, code_to_seq
+    r = get_disassembly(sel)
 
-    l = instruction.length
-    h = []
-    for _ in sel[offset: offset + l]:
-        h.append(" %02X" % ord(_))
-    h = ",".join(h)
-    asm = get_instruction_string(instruction, FORMAT_INTEL, offset)
-    r.append([h, asm])
-    offset += l
-hlen = max(len(i[0]) for i in r) + 1
+    hlen = max(len(i[0]) for i in r) + 1
 
-filename = "temp%s.txt" % os.getpid()
-f = open(filename, "wt")
+    import os
+    filename = "temp%s.txt" % os.getpid()
+    f = open(filename, "wt")
 
-f.write("Add an * in front of the bytes you want to ignore\n\n")
-f.write("\n".join(("%s //%s" % (i[0].ljust(hlen), i[1]) for i in r)))
-f.close()
-os.system(filename)
+    f.write("// Add a * in front of the bytes you want to ignore\n")
+    f.write("// this file will be deleted at the end of the procedure\n\n")
+    f.write("\n".join(("// %s:%s" % (i[0].ljust(hlen), i[1]) for i in r)))
+    f.close()
 
-#TODO: call asm2test
-#f = open(filename, "rt")
-#r = f.read()
-#f.close()
-#
-#os.remove(filename)
+    #hiew.MessageWaitOpen()
+    os.system(filename)
+    #hiew.MessageWaitClose()
+
+    f = open(filename, "rt")
+    r = f.readlines()
+    f.close()
+
+    f = open(filename, "wt")
+    for i in r:
+        f.write(i)
+    f.write("\n")
+    f.write(templatize(seq_to_snippets(code_to_seq(r))))
+    f.close()
+
+    #TODO: make portable hiew.MessageWaitOpen()
+    os.system(filename)
+    #hiew.MessageWaitClose()
+
+    #hiew.Window.FromString("Success", "Operation successfull! deleting temp file...")
+    os.remove(filename)
+
--- misc/utils.py
+++ misc/utils.py
@@ -1,20 +1,68 @@
+def get_disassembly(sel, start=None):
+    from pydasm import get_instruction, get_instruction_string, MODE_32, FORMAT_INTEL
+    if start is None:
+        start = 0
+    r = []
+    offset = 0
+    #TODO: get the source offset
+    while offset < len(sel):
+        instruction = get_instruction(sel[offset:], MODE_32)
+
+        l = instruction.length
+        h = []
+        for _ in sel[offset: offset + l]:
+            h.append("%02X" % ord(_))
+        h = ",".join(h)
+        asm = get_instruction_string(instruction, FORMAT_INTEL, offset + start)
+        r.append([h, asm])
+        offset += l
+    return r
+
+def getEPdata(fn, length=None):
+	import pefile
+	if length is None:
+		length = 30
+	pe = pefile.PE(fn)
+	ep = pe.OPTIONAL_HEADER.AddressOfEntryPoint
+	data = pe.get_memory_mapped_image()[ep:ep + length]
+	return data
+
+def getwildstring(s1,s2):
+    """takes 2 strings, returns a string made of the common bytes or question marks"""
+    l1 = len(s1)
+    l2 = len(s2)
+    if l1 < l2:
+        s1 = s1 + " " * (l2 - l1)
+    elif l1 > l2:
+        s2 = s2 + " " * (l1 - l2)
+
+    result = []
+    for i, j in enumerate(s1):
+        if s2[i] == j:
+            result.append(j)
+        else:
+            result.append("?")
+    return "".join(result)
+
 def code_to_seq(s):
-    """takes a list of string, remove comments, parse byte, replace *byte with None"""
+    """takes a list of string, remove comments, parse byte, replace *byte with None
+    @returns a list of string of hex bytes or None
+    """
     cleaned = []
     for l in s:
         # a code line should contain a // comment, otherwise skip it
-        off = l.find("//")
+        off = l.find(":")
         if off == -1:
             continue
 
         # strip unneded chars
-        l = l[:off].strip()
+        l = l[2:off].strip()
         l = l.replace(" ", "")
         cleaned += l.split(',')
 
     # replace *bytes with None
     for i,j in enumerate(cleaned):
-        if j.startswith("*"):
+        if j.lstrip().startswith("*") or j.lstrip().startswith("?"):
             cleaned[i] = None
     return cleaned
 
@@ -45,13 +93,13 @@ def templatize(seqs):
     for off, bytes in seqs:
         len_ = len(bytes)
         if len_ == 4:
-            text.append("\t (*(UINT32*)&buffer[i + %02x]             == 0x%s%s%s%s)" % (off, bytes[3], bytes[2], bytes[1], bytes[0]))
+            text.append("\t (*(UINT32*)&buffer[i + 0x%02x] == 0x%s%s%s%s)" % (off, bytes[3], bytes[2], bytes[1], bytes[0]))
         elif len_ == 3:
-            text.append("\t((*(UINT32*)&buffer[i + %02x] & 0xFFFFFF) == 0x%s%s%s)" % (off, bytes[2], bytes[1], bytes[0]))
+            text.append("\t((*(UINT32*)&buffer[i + 0x%02x] & 0x00FFFFFF) == 0x00%s%s%s)" % (off, bytes[2], bytes[1], bytes[0]))
         elif len_ == 2:
-            text.append("\t (*(UINT16*)&buffer[i + %02x]             == 0x%s%s)" % (off, bytes[1], bytes[0]))
+            text.append("\t (*(UINT16*)&buffer[i + 0x%02x] == 0x%s%s)" % (off, bytes[1], bytes[0]))
         elif len_ == 1:
-            text.append("\t (*(UINT8*) &buffer[i + %02x]             == 0x%s)" % (off, bytes[0]))
+            text.append("\t (*(UINT8*) &buffer[i + 0x%02x] == 0x%s)" % (off, bytes[0]))
 
     #enclose the conditions
     template = "if (\n%s\n\t)\n\t{" % " && \n".join(text)
