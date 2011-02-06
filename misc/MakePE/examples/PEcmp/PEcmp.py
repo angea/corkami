b0dc7887fe86ee517473c04b0edeb1c3421a6d1f
Ange Albertini <angea@users.noreply.github.com>
2011-02-06 16:10:23 +0000

    added binaries for PE comparer script

---
+++ misc/MakePE/examples/PEcmp/PEcmp.py
@@ -0,0 +1,7 @@
+"""check if TLS was added
+check if import was added
+check if EP is the same
+check if code at EP is the same
+try to check if internal calls are the same
+try to check if import trampolines are the same
+"""
