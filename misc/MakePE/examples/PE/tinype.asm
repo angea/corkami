36fabf699373ea626e13348d6291937e0a595a0f
Ange Albertini <angea@users.noreply.github.com>
2011-02-11 18:12:49 +0000

    cleaner code

--- misc/MakePE/examples/PE/tinype.asm
+++ misc/MakePE/examples/PE/tinype.asm
@@ -1,50 +1,73 @@
 ; a 97-byte TinyPE file, but less defined information (sections removed, and minor stuff)
 
 %include '..\..\consts.asm'
-%define round(n, r) (((n+(r-1))/r)*r)   ; can't get that working under yasm, have to hardcode values :(
 
-bits 32
+;specific to this file, a truncated Optional Header
+struc TRUNC_OPTIONAL_HEADER32
+  .Magic                        resw 1
+  .MajorLinkerVersion           resb 1
+  .MinorLinkerVersion           resb 1
+  .SizeOfCode                   resd 1
+  .SizeOfInitializedData        resd 1
+  .SizeOfUninitializedData      resd 1
+  .AddressOfEntryPoint          resd 1
+  .BaseOfCode                   resd 1
+  .BaseOfData                   resd 1
+  .ImageBase                    resd 1
+  .SectionAlignment             resd 1
+  .FileAlignment                resd 1
+  .MajorOperatingSystemVersion  resw 1
+  .MinorOperatingSystemVersion  resw 1
+  .MajorImageVersion            resw 1
+  .MinorImageVersion            resw 1
+  .MajorSubsystemVersion        resw 1
+  .MinorSubsystemVersion        resw 1
+  .Win32VersionValue            resd 1
+  .SizeOfImage                  resd 1
+  .SizeOfHeaders                resd 1
+  .CheckSum                     resd 1
+  .Subsystem                    resb 1  ; truncated as a byte
+  ; no more data
+endstruc
 
-SECTIONALIGN EQU 4
-FILEALIGN EQU 4
 IMAGEBASE equ 400000h
 
 org IMAGEBASE
 
-DOS_HEADER:
+DOS_HEADER:             ; mostly ignored, only e_magic and e_lfanew required
 .e_magic       dw 'MZ'
-times 2 db 0
 
-NT_SIGNATURE:
-    db 'PE',0,0
-FILE_HEADER:
-.Machine                dw IMAGE_FILE_MACHINE_I386
-dw 0
+align 4, db 0
 
+istruc IMAGE_NT_HEADERS
+    at IMAGE_NT_HEADERS.Signature, db 'PE',0,0
+iend
+istruc IMAGE_FILE_HEADER
+    at IMAGE_FILE_HEADER.Machine,               dw IMAGE_FILE_MACHINE_I386
+    ; we need to make sure it's not some crazy value in the NumbersOfSections, and the TimeStamp will fit the entrypoint code
+    at IMAGE_FILE_HEADER.TimeDateStamp
+
+bits 32
 EntryPoint:
     push byte 42
     pop eax
     ret
-CODESIZE equ $ - EntryPoint
 
-times 0ah db 0
-.Characteristics        dw IMAGE_FILE_RELOCS_STRIPPED | IMAGE_FILE_EXECUTABLE_IMAGE| IMAGE_FILE_LINE_NUMS_STRIPPED | IMAGE_FILE_LOCAL_SYMS_STRIPPED | IMAGE_FILE_32BIT_MACHINE
-OPTIONAL_HEADER:
-.Magic                  dw IMAGE_NT_OPTIONAL_HDR32_MAGIC
-times 14 db 0
-.AddressOfEntryPoint    dd EntryPoint - IMAGEBASE
-times 8 db 0
-.ImageBase              dd IMAGEBASE
-.SectionAlignment       dd SECTIONALIGN         ; wich defines also e_lfanew, to the right value
-.FileAlignment          dd FILEALIGN
-times 8 db 0
-.MajorSubsystemVersion  dw 4
-times 6 db 0
-.SizeOfImage            dd 100 + 4  ;rounding not working
-.SizeOfHeaders          dd 100      ;rounding not working - not necessary on all XP versions
-times 4 db 0
-.Subsystem              db IMAGE_SUBSYSTEM_WINDOWS_GUI ; shortened on a byte
+    at IMAGE_FILE_HEADER.Characteristics,       dw IMAGE_FILE_RELOCS_STRIPPED | IMAGE_FILE_EXECUTABLE_IMAGE| IMAGE_FILE_LINE_NUMS_STRIPPED | IMAGE_FILE_LOCAL_SYMS_STRIPPED | IMAGE_FILE_32BIT_MACHINE
+iend
+
+istruc TRUNC_OPTIONAL_HEADER32
+    at TRUNC_OPTIONAL_HEADER32.Magic,                     dw IMAGE_NT_OPTIONAL_HDR32_MAGIC
+    at TRUNC_OPTIONAL_HEADER32.AddressOfEntryPoint,       dd EntryPoint - IMAGEBASE
+    at TRUNC_OPTIONAL_HEADER32.ImageBase,                 dd IMAGEBASE
+    at TRUNC_OPTIONAL_HEADER32.SectionAlignment,          dd 4       ; also sets e_lfanew
+    at TRUNC_OPTIONAL_HEADER32.FileAlignment,             dd 4
+    at TRUNC_OPTIONAL_HEADER32.MajorSubsystemVersion,     dw 4
+    at TRUNC_OPTIONAL_HEADER32.SizeOfImage,               dd 100 + 4  ;rounding not working
+    at TRUNC_OPTIONAL_HEADER32.SizeOfHeaders,             dd 100      ;rounding not working - not necessary on all XP versions
+    at TRUNC_OPTIONAL_HEADER32.Subsystem,                 db IMAGE_SUBSYSTEM_WINDOWS_GUI
+iend
 SIZEOFHEADER equ $ - IMAGEBASE ; rounding not working
 
 ;Original TinyPE @ http://www.phreedom.org/solar/code/tinype/
-;Ange Albertini, Creative Commons BY, 2010
+;Ange Albertini, BSD Licence, 2010-2011
