5c52b49fd91ca9c04fc389c4c2c502910417ee89
Ange Albertini <angea@users.noreply.github.com>
2011-03-04 20:03:20 +0000

    added TLS to disable Int2E on win7 cleaned code, added alignments recursive TLS structure

--- misc/MakePE/examples/asm/getip.asm
+++ misc/MakePE/examples/asm/getip.asm
@@ -1,21 +1,36 @@
-; how to get EIP
+; how to get EIP:
+; * Call/pop
+; * FPU
+; * Int 2e
+; * Exceptions:
+;   * before the instruction
+;   * on the instruction
+;   * on the next instruction
+
+; the Int2e trick is disabled under Win7
 
 %include '..\..\onesec.hdr'
 
-EntryPoint:
-    call _call
-    call _int
-    call _fpu
-    call _seh
-    jmp good
+%macro _ 0
+    nop
+%endmacro
 
-_call:
+%macro _c 0
+    align 16, int3
+%endmacro
+
+%macro _d 0
+    align 16, db 0
+%endmacro
+
+_callpop:
     call $ + 5
 after_call:
     pop edx
     cmp edx, after_call
     jnz bad
     retn
+_c
 
 _int:
     int 02eh    ; also works with int 2c
@@ -23,6 +38,7 @@ after_int:
     cmp edx, after_int
     jnz bad
     retn
+_c
 
 _fpu:
     fnop
@@ -31,15 +47,7 @@ _fpu:
     cmp edx, _fpu
     jnz bad
     retn
-
-fpuenv:
-    .ControlWord           dd 0
-    .StatusWord            dd 0
-    .TagWord               dd 0
-    .DataPointer           dd 0
-    .InstructionPointer    dd 0
-    .LastInstructionOpcode dd 0
-    dd 0
+_c
 
 %macro _before 0
     %push SEH
@@ -47,6 +55,7 @@ fpuenv:
 %endmacro
 
 %macro _after 1
+_c
 %$handler:
     mov eax, [esp + exceptionHandler.pContext + 4]
     cmp dword [eax + CONTEXT.regEip], %1
@@ -67,28 +76,83 @@ on_the_instruction:
     mov [eax], eax
     jmp bad
     _after on_the_instruction
-nop
+_
     _before
     db 0f1h
 trigger_after_execution:
     jmp bad
     _after trigger_after_execution
-nop
+_
     _before
     push 302h           ; TF + 10b
     popf                ; will trigger after the following instruction is executed
     jmp bad
     _after bad
     retn
+_c
+
+TLS:
+    ; a TLS to patch out the Int2E trick, not Win7 compatible
+    pushad
+    mov eax, [fs:18h]
+    mov ecx, [eax + 030h]
+    xor eax, eax
+    or eax, [ecx + 0a8h]
+    shl eax,8
+    or eax, [ecx + 0a4h]
+    cmp eax, 0106h               ; Win7 ? probably need to tune that version check
+    jz W7
+_
+    popad
+    retn
+_
+W7:
+    mov byte [disable_me], 090h
+    mov dword [disable_me + 1], 090909090h
+    popad
+    retn
+_c
+
+EntryPoint:
+    call _callpop
+
+disable_me:
+    call _int
+    call _fpu
+    call _seh
+    jmp good
+_c
 
 %include '..\goodbad.inc'
-
+_c
 ;%IMPORT user32.dll!MessageBoxA
 ;%IMPORT kernel32.dll!ExitProcess
+
+_d
+
+fpuenv:
+    .ControlWord           dd 0
+    .StatusWord            dd 0
+    .TagWord               dd 0
+    .DataPointer           dd 0
+    .InstructionPointer    dd 0
+    .LastInstructionOpcode dd 0
+    dd 0
+_d
+
+Image_Tls_Directory32:
+    StartAddressOfRawData dd Characteristics
+    EndAddressOfRawData   dd Characteristics
+    AddressOfIndex        dd Characteristics
+    AddressOfCallBacks    dd SizeOfZeroFill
+;Callbacks: ; embedded structure
+    SizeOfZeroFill        dd TLS
+    Characteristics       dd 0
+_d
 
 ;%IMPORTS
 
 SECTION0SIZE equ $ - Section0Start
 SIZEOFIMAGE equ $ - IMAGEBASE
 
-;Ange Albertini, Creative Commons BY, 2010
+;Ange Albertini, BSD Licence, 2010-2011
