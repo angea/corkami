3ea739039bd3d08e5441662e17b36c4bc60099c8
Ange Albertini <angea@users.noreply.github.com>
2011-03-04 20:22:40 +0000

    added about to exploits added in progress map on pdfs

---
+++ mindmaps/PDFs.mm
@@ -0,0 +1,147 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1294307441609" ID="Freemind_Link_1008706029" MODIFIED="1299269968687" TEXT="PDF">
+<edge WIDTH="thin"/>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1294307451171" ID="_" MODIFIED="1294602405312" POSITION="right" TEXT="Structure">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1294307465187" FOLDED="true" ID="Freemind_Link_1992020892" MODIFIED="1294602401812" TEXT="Signature">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1294307507500" ID="Freemind_Link_1802939532" MODIFIED="1294602397000" TEXT="appended">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>(something before %PDF, even a complete binary...)</text>
+</hook>
+</node>
+<node CREATED="1294307519265" ID="Freemind_Link_108863326" MODIFIED="1294602397546" STYLE="fork" TEXT="truncated&#xa;%PDF-1.">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1294307611359" ID="Freemind_Link_420769718" MODIFIED="1294344591906" TEXT="null-truncated">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>%PDF-\00 also works</text>
+</hook>
+</node>
+</node>
+<node CREATED="1294307858656" ID="Freemind_Link_278310758" MODIFIED="1294602407171" TEXT="discardable elements">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1294307885453" ID="Freemind_Link_1141969726" MODIFIED="1294602398796" TEXT="%%EOF">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1294307903859" ID="Freemind_Link_1832673714" MODIFIED="1294602400000" TEXT="xref">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1294307931750" ID="Freemind_Link_684985235" MODIFIED="1294602398531" TEXT="object references">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1294308076250" ID="Freemind_Link_1865846308" MODIFIED="1294602400250" TEXT="endobj">
+<edge WIDTH="thin"/>
+</node>
+<node CREATED="1294308081281" ID="Freemind_Link_86704096" MODIFIED="1294602400750" TEXT="endstream">
+<edge WIDTH="thin"/>
+</node>
+<node CREATED="1294308084343" ID="Freemind_Link_1467141095" MODIFIED="1294602401015" TEXT="object length">
+<edge WIDTH="thin"/>
+</node>
+</node>
+<node CREATED="1294590890250" ID="Freemind_Link_243518677" MODIFIED="1294602406968" TEXT="incremental">
+<edge WIDTH="thin"/>
+</node>
+<node CREATED="1294590990593" ID="Freemind_Link_816178136" MODIFIED="1294605846812" TEXT="hex names">
+<edge WIDTH="thin"/>
+</node>
+<node CREATED="1299269930671" ID="Freemind_Link_1036207383" MODIFIED="1299269935484" TEXT="parsing direction"/>
+</node>
+<node CREATED="1294307456859" ID="Freemind_Link_211135620" MODIFIED="1294602411140" POSITION="right" TEXT="Javascript">
+<edge WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1294307961750" FOLDED="true" ID="Freemind_Link_855630163" MODIFIED="1294602412937" TEXT="string obfuscation">
+<edge WIDTH="thin"/>
+<node CREATED="1294307972546" ID="Freemind_Link_829906192" MODIFIED="1294602412921" TEXT="concat">
+<edge WIDTH="thin"/>
+</node>
+<node CREATED="1294307976218" ID="Freemind_Link_255673800" MODIFIED="1294602412890" TEXT="regexp">
+<edge WIDTH="thin"/>
+</node>
+<node CREATED="1294307979578" ID="Freemind_Link_1569013892" MODIFIED="1294602412859" TEXT="unescape">
+<edge WIDTH="thin"/>
+</node>
+<node CREATED="1294590824156" ID="Freemind_Link_562856246" MODIFIED="1294602412828" TEXT="eval">
+<edge WIDTH="thin"/>
+</node>
+<node CREATED="1294590857343" ID="Freemind_Link_716183506" MODIFIED="1294602412796" TEXT="base">
+<edge WIDTH="thin"/>
+</node>
+</node>
+<node CREATED="1294308108984" ID="Freemind_Link_353722632" MODIFIED="1294602412781" TEXT="anti-debugs">
+<edge WIDTH="thin"/>
+<node CREATED="1294308114000" ID="Freemind_Link_160368360" MODIFIED="1294602412750" TEXT="global variables">
+<edge WIDTH="thin"/>
+</node>
+<node CREATED="1294308129296" ID="Freemind_Link_198446928" MODIFIED="1294602412718" TEXT="specific functions">
+<edge WIDTH="thin"/>
+</node>
+<node CREATED="1294308139093" ID="Freemind_Link_1479337808" MODIFIED="1294602412703" TEXT="callee">
+<edge WIDTH="thin"/>
+</node>
+<node CREATED="1294590794921" ID="Freemind_Link_328481949" MODIFIED="1294602412687" TEXT="annots">
+<edge WIDTH="thin"/>
+</node>
+<node CREATED="1299269941531" ID="Freemind_Link_93976465" MODIFIED="1299269944171" TEXT="getfield"/>
+<node CREATED="1294590829578" ID="Freemind_Link_1395862968" MODIFIED="1294602412656" TEXT="array">
+<edge WIDTH="thin"/>
+</node>
+<node CREATED="1294590834343" ID="Freemind_Link_1558605342" MODIFIED="1294602412640" TEXT="info">
+<edge WIDTH="thin"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>author, producer, title</text>
+</hook>
+</node>
+<node CREATED="1294590896609" ID="Freemind_Link_681214787" MODIFIED="1294602412625" TEXT="XFA">
+<edge WIDTH="thin"/>
+</node>
+<node CREATED="1294590899375" ID="Freemind_Link_996574882" MODIFIED="1294602412609" TEXT="getPageNthWord">
+<edge WIDTH="thin"/>
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>getPageNthWordQuads</text>
+</hook>
+</node>
+<node CREATED="1294590932359" ID="Freemind_Link_19664670" MODIFIED="1294602412578" TEXT="timers">
+<edge WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node CREATED="1294591046968" ID="Freemind_Link_880316926" MODIFIED="1294602385031" POSITION="left" TEXT="about">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1294591148671" ID="Freemind_Link_1575762527" MODIFIED="1294605932562" TEXT="author">
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>Ange Albertini</text>
+</hook>
+</node>
+<node CREATED="1294591543781" ID="Freemind_Link_424563649" LINK="mailto:ange.albertini@gmail.com" MODIFIED="1294602460390" TEXT="e-mail"/>
+<node CREATED="1294591201046" ID="Freemind_Link_677907833" LINK="http://corkami.blogspot.com" MODIFIED="1294591519343" TEXT="URL"/>
+<node CREATED="1294591206203" ID="Freemind_Link_350717320" MODIFIED="1294591337593" TEXT="credits">
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>Didier Stevens&#xa;Kazumasa Itabashi&#xa;Marco Cova&#xa;Sebastian Porst&#xa;</text>
+</hook>
+</node>
+</node>
+<node CREATED="1294308058765" ID="Freemind_Link_726870073" MODIFIED="1294324132890" POSITION="left" TEXT="whitespace"/>
+<node CREATED="1294590715500" ID="Freemind_Link_166315238" MODIFIED="1294591009484" POSITION="left" TEXT="stream encoding">
+<node CREATED="1294590718703" FOLDED="true" ID="Freemind_Link_387457832" MODIFIED="1294590720000" TEXT="hex">
+<node CREATED="1294590721000" ID="Freemind_Link_1600948373" MODIFIED="1294590725906" TEXT="with whitespace"/>
+</node>
+<node CREATED="1294590726796" ID="Freemind_Link_1910764948" MODIFIED="1294590728437" TEXT="octal"/>
+<node CREATED="1294590729484" ID="Freemind_Link_607446402" MODIFIED="1294590731937" TEXT="newline"/>
+</node>
+</node>
+</map>
