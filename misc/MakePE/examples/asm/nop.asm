c67d3f1fc1bdbbd0f8feb072738d3b08b9190343
Ange Albertini <angea@users.noreply.github.com>
2011-03-22 20:30:48 +0000

    Nop: added description, simple randomization start      added ?fence opcodes_desc: 	added informative header, better paragraph separation, some extra comments 	opposite setc/salc/cmovc case 	removed nops 	added popcnt, movbe, preliminary crc32 	added XP/W7 specific values, with TLS detection

--- misc/MakePE/examples/asm/nop.asm
+++ misc/MakePE/examples/asm/nop.asm
@@ -1,20 +1,67 @@
+; nop has several interpretations, depending on its true use.
+; the true nop, that only consumes one byte, and changes nothing but EIP in the CPU state.
+; then, a nop for heap spray is just an uninmportant (it won't fail to execute) opcode that just takes one byte
+; then, changing only the standard flags can be seen as a nop.
+; or changing only the first entry on the stack...
+; in standard forms of obfuscation, changing something then reverting the changes - and only having modified the stack or the flags
+; thus by extension, a 'NOP' is one or a sequence of opcodes, that didn't change the standard registers nor the memory.
+
+; changing and reverting the memory is typically NOT seen as a nop, as it could be intercepted by a breakpoint.
+
 %include '..\..\onesec.hdr'
 
+%macro rand 1
+    rdtsc
+    lea %1, [eax + edx * 8]
+%endmacro
+
+randomize:
+    rand ebx
+    rand ecx
+    rand esi
+    rand edi
+    rand ebp
+    retn
 
 EntryPoint:
+    call randomize
     pushad
 _
     nop
     xchg eax, eax
-    mov eax, eax
     xchg al, al
     pause
+_
+    mov eax, eax
+    and eax, eax                            ; should clear Z
+    or eax, eax
+    xor eax, 0
+_
+    cmc
+    stc
+    clc
+    std
+    cld
+;loopz _afterloop
+;_afterloop:
+jnz _afterjnz
+_afterjnz:
+cmovz eax, ebx
+
+
+_
+    sfence
+    mfence
+    lfence
+    prefetchnta [eax]   ; no matter the eax value
+_
+    ; if OF is not set, this just does a nop - a risky nop then
+    into
 _
     db 0fh, 1ch, 00                         ; nop [eax] ; doesn't trigger an exception
     db 0fh, 019h, 084h, 0c0h
         dd 080000000h                       ; hint_nop [eax + eax * 8 - 080000000h]
 _
-
     push eax
     pop eax
 _
@@ -24,32 +71,35 @@ _
     enter 0,0
     leave
 _
+    pushad
+    popad
+    pushaw
+    popaw
+_
     pushf
     popf
+    pushfw
+    popfw
+_
     push ds
     pop ds
-
 _
     inc eax
     dec eax
 
-    nop
-    xchg eax, eax
-    xchg al, al
-_
-    db 0fh, 1ch, 00                         ; nop [eax] ; doesn't trigger an exception
-    db 0fh, 019h, 084h, 0c0h
-        dd 080000000h                       ; hint_nop [eax + eax * 8 - 080000000h]
-_
+    add eax, 31415926h
+    sub eax, 31415926h
 
+    lea eax, [eax + 31415926h]
+    sub eax, 31415926h
+
+    rol eax, 35
+    ror eax, 35
+_
     fnop
     emms
 _
-
     ;int 2dh
-_
-    ; if OF is not set, this just does a nop
-    into
 _
     cmp eax, [esp + 1ch]
     jnz bad
@@ -59,17 +109,14 @@ _
     jnz bad
     cmp ebx, [esp + 10h]
     jnz bad
-;    cmp esp, [esp + 0ch]
-;    jnz bad
     cmp ebp, [esp + 8]
     jnz bad
     cmp esi, [esp + 4]
     jnz bad
     cmp edi, [esp + 0]
     jnz bad
-_
     popad
-
+_
     jmp good
 _c
 
--- misc/MakePE/examples/asm/opcodes_desc.asm
+++ misc/MakePE/examples/asm/opcodes_desc.asm
@@ -1,4 +1,14 @@
-;small code using each opcode
+;a file making use of each user mode opcodes 
+
+; FPU/SSE+ are not included
+; Jumps specific opcodes are in Jumps.
+; opcodes doing 'nothing visible are in Nops.asm
+
+; the classic: mov movzx movsx lea xchg inc dec or and xor not rol ror rcl rcr shl shr add adc sub sbb div mul imul
+; the not so classic: aaa daa aas das aad aam xadd sar enter leave bsf btX bswap cbw cwde cwd rdtsc setXX salc cmovXX shld shrd 
+; the rares: lds bound xlatb cmpxchg cmpxchg8b cpuid lsl popcnt movbe crc32 arpl lar verr sldt sidt sgdt str
+
+;Ange Albertini, BSD Licence, 2009-2011
 
 %include '..\..\onesec.hdr'
 
@@ -23,175 +33,173 @@ db 0f1h
 EntryPoint:
     mov eax, 3
     expect eax, 3
-
+_
     mov al, -1
     movzx ecx, al
     expect ecx, 0ffh
-
+_
     mov al, -3
     movsx ecx, al
     expect ecx, -3
-
+_
     mov eax, 3
-    lea eax, [eax * 4 + 203Ah]
-    expect eax, 203ah + 4 * 3
-
+    lea eax, [eax * 8 + eax + 203Ah]
+    expect eax, 203ah + 8 * 3 + 3
+_
     mov al, 1
     mov bl, 2
     xchg al, bl
     expect al, 2
     expect bl, 1
-
+_
     xchg [val] , esp    ; makes a backup of ESP and temporarily change ESP to the start of the data
     popad               ; read all the data into registers
     mov esp, [val]       ; restore ESP and EAX
-
+_
     mov ax, 0304h ; 34
     mov bx, 0307h ; 37
     add ax, bx
     aaa
     expect ax, 0701h ;34 + 37 = 71
-
+_
     mov ax, 01234h ; 1234
     mov bx, 0537h  ; 537
     add ax, bx
     daa
     expect ax, 1771h ; 1234 + 537 = 1771
-
+_
     mov al, 01h
     mov bl, 04h
     sub al, bl
     aas
     expect al, 11 - 4
-
+_
     mov eax, 01771h
     mov ebx, 01234h
     sub eax, ebx
     das
-    expect eax, 537h
-
+    expect eax, 537h ; 1771 - 1234 = 537
+_
     mov ax, 305h
     aad
     expect ax, 35
-
+_
     mov ax, 35
     aam
     expect ax, 305h
-
+_
     mov eax, 3
     add eax, 3
     expect eax, 6
-
+_
     mov al, 1
     mov bl, 2
     xadd al, bl
     expect al, 1 + 2
     expect bl, 1
-
+_
     stc
     mov eax, 3
     adc eax, 3
     expect eax, 3 + 3 + 1
-
+_
     mov eax, 6
     sub eax, 3
     expect eax, 6 - 3
-
+_
     stc
     mov eax, 6
     sbb eax, 3
     expect eax, 6 - 3 - 1
-
+_
     mov eax, 0
     inc eax
     expect eax, 0 + 1
-
+_
     mov eax, 7
     dec eax
     expect eax, 7 - 1
-
+_
     mov eax, 1010b
     or eax, 0110b
     expect eax , 1110b
-
+_
     mov eax, 1010b
     and eax, 0110b
     expect eax, 0010b
-
+_
     mov eax, 1010b
     xor eax, 0110b
     expect eax, 1100b
-
+_
     mov al, 1010b
     not al
     expect al, 11110101b
-
+_
     mov eax, 1010b
     rol eax, 3
     expect eax, 1010000b
-
+_
     mov al, 1010b
     ror al, 3
     expect al, 01000001b
-
+_
     stc
     mov al, 1010b
     rcl al, 3
     expect eax, 1010100b
-
+_
     stc
     mov al, 1010b
     rcr al, 3
     expect al, 10100001b
-
+_
     mov al, 1010b
     shl al, 2
     expect al, 101000b
-
+_
     mov al, 1010b
     shr al, 2
     expect al, 10b
-
+_
     mov al, -8
     sar al, 2
     expect al, -2
-
+_
     mov ax, 1111b
     mov bx, 0100000000000000b
     shld ax, bx, 3
     expect ax, 1111010b
-
+_
     mov ax, 1101001b
     mov bx, 101b
     shrd ax, bx, 3
     expect ax, 1010000000001101b
-
+_
     mov ax, 35
     mov bl, 11
     div bl          ; 35 = 3 * 11 + 2
     expect al, 3    ; quo
     expect ah, 2    ; rem
-
+_
     mov al, 11
     mov bl, 3
     mul bl
-    expect ax, 33
-
+    expect ax, 3 * 11
+_
     mov eax, 11
     imul eax, eax, 3
-    expect eax, 33
-
+    expect eax, 3 * 11
+_
     push ds
-
-    mov ebx, addseg
+    mov ebx, addseg                         ; [addseg] = 00:12345678
     lds eax, [ebx]
     expect eax , 12345678h
     push ds
     pop eax
     expect eax, 0
-
     pop ds
-
+_
     push 3
     enter 8, 0
     enter 4, 1
@@ -199,34 +207,44 @@ EntryPoint:
     leave
     pop eax
     expect eax, 3
-
+_
     stc
     setc al
     expect al, 1
-
+    clc
+    setc al
+    expect al, 0
+_
     stc
     salc
     expect al, -1
-
     clc
-    mov eax, 0
+    salc
+    expect al, 0
+_
+    rdtsc
+    mov ecx, eax
+    clc
     mov ebx, 3
     cmovc eax, ebx
-    expect eax, 0
-
+    expect eax, ecx
+    stc
+    cmovc eax, ebx
+    expect eax, 3
+_
     mov eax, 0010100b
-    bsf ebx, eax
+    bsf ebx, eax                            ; bit scan forward
     expect ebx, 2
-
+_
     mov ax, 00100b
     mov bx, 2
-    bt ax,bx
+    bt ax,bx                                ; bit test
     jnc bad
-
+_
     mov eax, 12345678h
     bswap eax
     expect eax, 78563412h
-
+_
     mov eax, -1
     mov al, 3
     cbw
@@ -235,54 +253,63 @@ EntryPoint:
     expect eax, 3
     cwd
     expect dx, 0
-
+_
     mov eax, 136
-    mov ebx, boundslimit
+    mov ebx, boundslimit                    ; boundslimit = [135, 143]
     bound eax, [ebx]
-
+    ; no exception happens if within bounds
+_
     ; compares lower 2 bits and copy if inferior
     CONST equ 1111111111111100b
     mov ax, CONST
     mov bx, 1010100111b
     arpl ax, bx
-    jnz bad             ; ZF should be set too
+    jnz bad                                 ; ZF should be set too
     expect ax, CONST + 11b
-
+_
     mov ecx, -1
-    db 66h      ; just increasing cx instead of ecx
+    db 66h                                  ; just increasing cx instead of ecx
     inc ecx
     expect ecx, 0ffff0000h
-
-    db 67h      ; just checking cx instead of ecx
+_
+    db 67h                                  ; just checking cx instead of ecx
     jecxz _cx0
     jmp bad
 _cx0:
 
     mov al, 35
-    mov ebx, xlattable
+    mov ebx, xlattable                      ; xlattable[35] = 75
     xlatb
     expect al, 75
-
+_
     push cs
     pop ecx
     lar eax, ecx
     expect eax, 0cffb00h
-
+_
     push cs
     pop ecx
     verr cx
     jnz bad
-
+_
     mov al, 3
     mov bl, 6
     cmpxchg bl, cl
     expect al, bl
-
+_
     mov al, 3
     mov bl, al
     cmpxchg bl, cl
     expect bl, cl
-
+_
+    mov eax, 00a0a0a0ah
+    mov edx, 0d0d0d0d0h
+    mov ecx, 99aabbcch
+    mov ebx, 0ddeeff00h
+    cmpxchg8b [cmpxchg8bme]                 ; [cmpxchg8bme] = 0d0d0d0d0:00a0a0a0a
+    expect [cmpxchg8bme], ebx
+    expect [cmpxchg8bme + 4], ecx
+_
     rdtsc
     mov ebx, eax
     mov ecx, edx
@@ -290,85 +317,155 @@ _cx0:
     cmp eax, ebx
     jle bad
     expect edx, ecx
-
+_
     sldt eax
     expect eax, 0
-
+_
     mov eax, 0
     cpuid
     test eax, eax
     jz bad
-
-    ; nops
-    sfence
-    mfence
-    lfence
-    prefetchnta [eax]   ; no matter the eax value
-    db 0fh, 019h, 00    ; hint nop [eax]
-    db 0fh, 01fh, 00    ; nop [eax]
-
+_
     push cs
     pop ecx
     lsl eax, ecx
     jnz bad
     expect eax, -1
+_
+    cpuid
+    and ecx, 1 << 23
+    jz no_popcnt
+    mov ebx, 0100101010010b
+    popcnt eax, ebx
+    expect eax, 5
+no_popcnt:
+_
+    cpuid
+    and ecx, 1 << 22
+    jz no_movbe
 
-    jmp good            ; the remaining tests are XP specific
+    mov ebx, _movbe                         ; [_movbe] = 11223344h
+    movbe eax, [ebx]
+    expect eax, 44332211h
+no_movbe:
+_
+    cpuid
+    and ecx, 1 << 20
+    jz no_crc32
+    mov eax, 0
+    mov ebx, crc32_
+    crc32 eax, [ebx]
+    ; expect eax, 5
+no_crc32:
+_
+    jmp [os]
+_c
 
+XP_tests:
     mov eax, dummy
     sidt [eax]
-;    expect word [eax],  007ffh ; 0fffh under win7 x64
-
+    expect word [eax], 007ffh
+_
     mov eax, dummy
     sgdt [eax]
-;    expect word [eax],  003ffh ; 07fh under win7 x64
-
+    expect word [eax], 003ffh
+_
     str ax
-;    expect ax, 28h ; 40h under win7 x64
-
+    expect ax, 28h
+_
     mov eax, 0
     push _return
     mov edx, esp
-    sysenter        ; not under win7 x64
+    sysenter
 _return:
     expect eax, ACCESS_VIOLATION            ; depends on initial EAX
     lea eax, [esp - 4]
-    expect ecx, eax                             ; 1 if stepping
+    expect ecx, eax                         ; 1 if stepping
     mov al, [edx]
     expect al, 0c3h
-    expect edx, [__imp__KiFastSystemCallRet]    ; -1 if stepping
-
+    expect edx, [__imp__KiFastSystemCallRet]; -1 if stepping
     jmp good
-
-align 4, db 0
+_c
 ;%IMPORT ntdll.dll!KiFastSystemCallRet
+_c
+W7_tests:
+    mov eax, dummy
+    sidt [eax]
+    expect word [eax], 0fffh
+_
+    mov eax, dummy
+    sgdt [eax]
+    expect word [eax], 07fh
+_
+    str ax
+    expect ax, 40h
+    jmp good
+_c
 xlattable:
 times 35 db 0
-db 75
+         db 75
 
-    dd 1
 boundslimit:
     dd 135
     dd 143
+_d
+
+crc32_:
+    dd 12345678h
+_d
+cmpxchg8bme:
+    dd 00a0a0a0ah
+    dd 0d0d0d0d0h
+_d
+
 addseg:
     dd 12345678h
-    dw 00h         ; standard value for DS
+    dw 00h
+_d
 
-align 4, db 0
+_movbe dd 11223344h
+_d
 
 %include '..\goodbad.inc'
+_c
 
 ;%IMPORT user32.dll!MessageBoxA
 ;%IMPORT kernel32.dll!ExitProcess
+_c
+TLS:
+    mov eax, [fs:18h]
+    mov ecx, [eax + 030h]
+    xor eax, eax
+    or eax, [ecx + 0a8h]
+    shl eax,8
+    or eax, [ecx + 0a4h]
+    cmp eax, 0106h
+    jz W7
+    retn
+W7:
+    mov dword [os], W7_tests
+    retn
+_c
+
+Image_Tls_Directory32:
+    StartAddressOfRawData dd Characteristics
+    EndAddressOfRawData   dd Characteristics
+    AddressOfIndex        dd Characteristics
+    AddressOfCallBacks    dd SizeOfZeroFill
+    SizeOfZeroFill        dd TLS
+    Characteristics       dd 0
+_d
 
 dummy dd 0,0
+os dd XP_tests
+_d
 
 tada db "Tada!", 0
 helloworld db "Hello World!", 0
+_d
 
 ;%IMPORTS
+_d
 
 SECTION0SIZE equ $ - Section0Start
 SIZEOFIMAGE equ $ - IMAGEBASE
-
-;Ange Albertini, Creative Commons BY, 2009-2010
