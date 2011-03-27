d521fe9fa94ba8681324ed096f9667078718f0de
Ange Albertini <angea@users.noreply.github.com>
2011-03-27 11:35:47 +0000

    split categories into calls     added nops encodings undocumented disassembly put together cpuids dependant opcodes put back fpu, changed order after op specifics merged gs, smws, rdtsc, anti debugs added neg, bsr fixed sal

--- misc/MakePE/examples/asm/usermode_test.asm
+++ misc/MakePE/examples/asm/usermode_test.asm
@@ -1,13 +1,12 @@
+;TODO: gather code and data together, FS: MOVS, LOCKS, ud1, ud2, BTxx
+
 ; this a file making use of each usermode opcode (at least, one of each family)
 
-; FPU/SSE+ are not included, unless special cases like fstenv
-; Jumps specific opcodes are in Jumps.asm
-; opcodes doing nothing visible are in Nops.asm
-; opcodes triggering exceptions are in Seh_triggers.asm
+; general FPU/SSE+ opcodes are not included
 
-; the classic ones: mov movzx movsx lea xchg inc dec or and xor not rol ror rcl rcr shl shr add adc sub sbb div mul imul
-; the not so classic ones: aaa daa aas das aad aam xadd sar enter leave bsf btX bswap cpuid cbw cwde cwd rdtsc setXX salc cmovXX shld shrd
-; the rare ones: lds bound xlatb cmpxchg cmpxchg8b lsl popcnt movbe crc32 arpl lar verr sldt sidt sgdt str
+
+; anti-debug: rdtsc,
+; get ip:
 
 ; the OS dependent checks will fail under a VmWare
 
@@ -25,6 +24,43 @@ _CS equ 01bh ; cs is 01bh on Windows XP usermode, will fail if different
 %endmacro
 
 EntryPoint:
+    ; jump short, near, far, ret near, ret far, interrupt ret
+    call jumps
+
+    ; mov movzx movsx lea xchg add sub sbb adc inc dec or and xor
+    ; not neg rol ror rcl rcr shl shr sal shld shrd div mul imul enter leave
+    ; setXX cmovXX bsf bsr bt bswap cbw cwde cwd
+    call classics
+
+    ; xadd aaa daa aas das aad aam lds bound arpl jcxz xlatb lar
+    ; verr cmpxchg cmpxchg8b sldt lsl                           
+    call rares    
+                  
+
+    call undocumented ; aam xx, salc, aad xx, bswap reg16, smsw reg32
+
+    call cpu_specifics  ; popcnt movbe crc32
+
+    call encodings      ; test, 'sal'
+
+    ; os should be before any fpu use
+    call [os]
+
+    ; nop pause sfence mfence lfence prefetchnta 'hint nop', into
+    call nops
+
+    ; gs, smsw, rdtsc, pushf, pop ss
+    call antis
+
+    call get_ips ; call, call far, fstenv
+
+    ; documented but frequent disassembly mistakes
+    ; smsw str hints word calls/rets
+    call disassembly
+    jmp good
+_c
+
+jumps:
     jmp short _jmp1     ; short jump, relative, EB
 _c
 
@@ -33,8 +69,8 @@ _jmp1:
 _c
 
 _jmp2:                  ; jump via register
-    mov edi, _jmp3
-    jmp edi
+    mov eax, _jmp3
+    jmp eax
 _c
 
 _jmp3:
@@ -72,11 +108,14 @@ _c
 _pushiret:
     pushfd
     push cs
-    push _mov
+    push _ret
     iretd
 _c
 
-_mov:
+_ret:
+    ret
+
+classics:
     mov eax, 3
     expect eax, 3
 _
@@ -102,60 +141,9 @@ _
     popad                                   ; read all the data into registers
     mov esp, [xchgpopad]                    ; restore ESP and EAX
 _
-    mov ax, 0304h                           ; 34
-    mov bx, 0307h                           ; 37
-    add ax, bx
-    aaa
-    expect ax, 0701h                        ;34 + 37 = 71
-_
-    mov ax, 01234h                          ; 1234
-    mov bx, 0537h                           ; 537
-    add ax, bx
-    daa
-    expect ax, 1771h                        ; 1234 + 537 = 1771
-_
-    mov al, 01h
-    mov bl, 04h
-    sub al, bl
-    aas
-    expect al, 11 - 4
-_
-    mov eax, 01771h
-    mov ebx, 01234h
-    sub eax, ebx
-    das
-    expect eax, 537h                        ; 1771 - 1234 = 537
-_
-    mov ax, 0305h
-    aad
-    expect ax, 35                           ; 03 05 becomes 35
-_
-    ; undocumented: aad with an immediate operand that is not 10
-    rdtsc
-    mov ax, 0325h
-    aad 7                                   ; ah = 0, al = ah * 7 + al => al = 3Ah
-    expect ax, 003Ah
-_
-    rdtsc
-    mov ax, 35
-    aam
-    expect ax, 305h
-_
-    ; undocumented behavior with an immediate operand different from 10
-    rdtsc
-    mov al, 3ah
-    aam 3                                   ; ah = al / 3, al = al % 3 => ah = 13h, al = 1
-    expect ax, 1301h
-_
     mov eax, 3
     add eax, 3
     expect eax, 6
-_
-    mov al, 1
-    mov bl, 2
-    xadd al, bl
-    expect al, 1 + 2
-    expect bl, 1
 _
     stc
     mov eax, 3
@@ -195,6 +183,10 @@ _
     not al
     expect al, 11110101b
 _
+    mov al, 1010b
+    neg al
+    expect al, -1010b
+_
     mov eax, 1010b
     rol eax, 3
     expect eax, 1010000b
@@ -217,15 +209,9 @@ _
     shl al, 2
     expect al, 101000b
 _
-    ; undocumented. SAL is the same as SHL, but different encoding
-    mov al, 1010b
-    db 0c0h, 0f0h, 2                        ; sal al, 2
-    expect al, 101000b
-_
-    ; now on eax
-    mov al, 1010b
-    db 0c1h, 0f0h, 3                        ; sal eax, 2
-    expect al, 1010000b
+    mov eax, -1010b
+    sal eax, 3
+    expect eax, -1010000b
 _
     mov al, 1010b
     shr al, 2
@@ -260,15 +246,6 @@ _
     imul eax, eax, 3
     expect eax, 3 * 11
 _
-    push ds
-    mov ebx, addseg                         ; [addseg] = 00:12345678
-    lds eax, [ebx]
-    expect eax , 12345678h
-    push ds
-    pop eax
-    expect ax, 0
-    pop ds
-_
     push 3
     enter 8, 0
     enter 4, 1
@@ -282,15 +259,6 @@ _
     expect al, 1
     clc
     setc al
-    expect al, 0
-_
-    ; 'undocumented' opcode
-    stc
-    salc
-    expect al, -1
-_
-    clc
-    salc
     expect al, 0
 _
     rdtsc
@@ -307,6 +275,10 @@ _
     bsf ebx, eax                            ; bit scan forward
     expect ebx, 2
 _
+    mov eax, 0010100b
+    bsr ebx, eax                            ; bit scan reverse
+    expect ebx, 4
+_
     mov ax, 00100b
     mov bx, 2
     bt ax,bx                                ; bit test
@@ -315,13 +287,6 @@ _
     mov eax, 12345678h
     bswap eax
     expect eax, 78563412h
-_
-    ; undocumented bswap behavior on 16bit
-    mov eax, 12345678h
-    PREFIX_OPERANDSIZE
-    bswap eax                               ; bswap ax = xor ax, ax
-    cmp eax, 12340000h
-    jnz bad
 _
     mov eax, -1
     mov al, 3
@@ -332,18 +297,70 @@ _
     cwd
     expect dx, 0
 _
+    retn
+_c
+
+rares:
+    mov al, 1
+    mov bl, 2
+    xadd al, bl
+    expect al, 1 + 2
+    expect bl, 1
+_
+    mov ax, 0304h                           ; 34
+    mov bx, 0307h                           ; 37
+    add ax, bx
+    aaa
+    expect ax, 0701h                        ;34 + 37 = 71
+_
+    mov ax, 01234h                          ; 1234
+    mov bx, 0537h                           ; 537
+    add ax, bx
+    daa
+    expect ax, 1771h                        ; 1234 + 537 = 1771
+_
+    mov al, 01h
+    mov bl, 04h
+    sub al, bl
+    aas
+    expect al, 11 - 4
+_
+    mov eax, 01771h
+    mov ebx, 01234h
+    sub eax, ebx
+    das
+    expect eax, 537h                        ; 1771 - 1234 = 537
+_
+    mov ax, 0305h
+    aad
+    expect ax, 35                           ; 03 05 becomes 35
+_
+    rdtsc
+    mov ax, 35
+    aam
+    expect ax, 305h
+_
+    push ds
+    mov ebx, addseg                         ; [addseg] = 00:12345678
+    lds eax, [ebx]
+    expect eax , 12345678h
+    push ds
+    pop eax
+    expect ax, 0
+    pop ds
+_
     mov eax, 136
     mov ebx, boundslimit                    ; boundslimit = [135, 143]
     bound eax, [ebx]
     ; no exception happens if within bounds
 _
     ; compares lower 2 bits and copy if inferior
-    CONST equ 1111111111111100b
-    mov ax, CONST
+    ARPL_ equ 1111111111111100b
+    mov ax, ARPL_
     mov bx, 1010100111b
     arpl ax, bx
     jnz bad                                 ; ZF should be set too
-    expect ax, CONST + 11b
+    expect ax, ARPL_ + 11b
 _
     mov ecx, -1
     db 66h                                  ; just increasing cx instead of ecx
@@ -389,33 +406,8 @@ _
     expect [_cmpxchg8b], ebx
     expect [_cmpxchg8b + 4], ecx
 _
-    rdtsc
-    mov ebx, eax
-    mov ecx, edx
-    rdtsc
-    cmp eax, ebx
-    jle bad
-    expect edx, ecx
-_
-    call $ + 5
-after_call:
-    pop eax
-    expect eax, after_call
-_
-    call far 01bh: $ + 7
-after_far:
-    pop eax
-    expect eax, after_far
-    pop eax
-    expect eax, _CS
-_
     sldt eax
     expect eax, 0                           ; 4060 under VmWare
-_
-    mov eax, 0
-    cpuid
-    test eax, eax
-    jz bad
 _
     push cs
     pop ecx
@@ -423,37 +415,90 @@ _
     jnz bad
     expect eax, -1                          ; 0ffbfffffh under vmware
 _
+    retn
+_c
+
+undocumented:
+    ; undocumented behavior with an immediate operand different from 10
+    rdtsc
+    mov al, 3ah
+    aam 3                                   ; ah = al / 3, al = al % 3 => ah = 13h, al = 1
+    expect ax, 1301h
+_
+    ; 'undocumented' opcode: salc/setalc    ; Set AL on Carry.
+    stc
+    salc
+    expect al, -1
+_
+    clc
+    salc
+    expect al, 0
+_
+    ; aad with an immediate operand that is not 10
+    rdtsc
+    mov ax, 0325h
+    aad 7                                   ; ah = 0, al = ah * 7 + al => al = 3Ah
+    expect ax, 003Ah
+_
+    ; bswap behavior on 16bit
+    mov eax, 12345678h
+    PREFIX_OPERANDSIZE
+    bswap eax                               ; bswap ax = xor ax, ax
+    expect eax, 12340000h
+_
+    ; smsw on dword is officially undocumented, but it just fills the whole CR0 to the operand
+    smsw eax
+    expect eax, 08001003bh  ; XP
+
+    retn
+_c
+
+
+cpuid_ecx dd 0
+
+cpu_specifics:
+    mov eax, 0
+    cpuid
+    cmp eax, 0ah
+    jl bad
+_
     mov eax, 1
     cpuid
+    mov [cpuid_ecx], ecx
+
+    mov ecx, [cpuid_ecx]
     and ecx, 1 << 23
     jz no_popcnt
-
+_
     mov ebx, 0100101010010b
     popcnt eax, ebx
     expect eax, 5
 no_popcnt:
 _
-    mov eax, 1
-    cpuid
+    mov ecx, [cpuid_ecx]
     and ecx, 1 << 22
     jz no_movbe
-
+_
     mov ebx, _movbe                         ; [_movbe] = 11223344h
     movbe eax, [ebx]
     expect eax, 44332211h
 no_movbe:
 _
-    mov eax, 1
-    cpuid
+    mov ecx, [cpuid_ecx]
     and ecx, 1 << 20
     jz no_crc32
-
+_
     mov eax, 0abcdef9h
     mov ebx, 12345678h
     crc32 eax, ebx
     expect eax, 0c0c38ce0h
 no_crc32:
 _
+    retn
+_c
+
+
+nops:
 %macro rand 1
     rdtsc
     lea %1, [eax + edx * 8]
@@ -478,11 +523,6 @@ _
     sub eax, 0
     cmp eax, 0
     test eax, 0
-    ; alternate encodings
-    db 0f7h, 0c8h                           ; test eax, 0
-        dd 0
-    db 0f7h, 0c0h                           ; test eax, 0
-        dd 0
     and eax, eax                            ; should clear Z
 _
     cmc
@@ -501,7 +541,7 @@ _
     lfence
     prefetchnta [eax]   ; no matter the eax value
 _
-    ; if OF is not set, this just does a nop - a risky nop then
+    ; if OF is not set, this just does a nop - a tricky nop then
     into
 _
     db 0fh, 1ch, 00                         ; nop [eax] ; doesn't trigger an exception
@@ -542,11 +582,24 @@ _
     rol eax, 35
     ror eax, 35
 _
-;    fnop
-;    emms
+    fnop
+    emms
+_
+    ffreep st0
+    db 0d9h, 0d8h                           ; fstp1 st0
+    db 0dch, 0d0h                           ; fcom2 st0
+    db 0dch, 0d8h                           ; fcomp3 st0
+    db 0ddh, 0c8h                           ; fxch4 st0
+    db 0deh, 0d0h                           ; fcomp5 st0
+    db 0dfh, 0c8h                           ; fxch7 st0
+    db 0dfh, 0d0h                           ; fstp8 st0
+    db 0dfh, 0d8h                           ; fstp9 st0
+    db 0dbh, 0e0h                           ; fneni
+    db 0dbh, 0e1h                           ; fndisi
 _
     ;int 2dh
 _
+    ; we're checking nothing was modified during the nop pairs
     cmp eax, [esp + 1ch]
     jnz bad
     cmp ecx, [esp + 18h]
@@ -562,13 +615,13 @@ _
     cmp edi, [esp + 0]
     jnz bad
     popad
-
-    jmp [os]
+_
+    retn
 _c
 
 XP_tests:
-    smsw eax
-    expect eax, 08001003bh  ; XP
+    smsw ax
+    expect ax, 0003bh  ; XP
 _
     mov eax, sidt_
     sidt [eax]
@@ -578,8 +631,13 @@ _
     sgdt [eax]
     expect word [eax], 003ffh               ; 0412fh under vmware
 _
+    mov eax, -1
     str ax
-    expect ax, 28h                          ; 4000h under vmware
+    expect eax, 0ffff0028h                  ; 4000h under vmware
+_
+    mov eax, -1
+    str eax
+    expect eax, 28h                          ; 4000h under vmware
 _
     mov eax, 0
     push _return
@@ -594,17 +652,144 @@ _return:
     mov al, [edx]
     expect al, 0c3h
     expect edx, [__imp__KiFastSystemCallRet]; -1 if stepping
+    retn
+_c
+;%IMPORT ntdll.dll!KiFastSystemCallRet
+_c
+
+CONST equ 035603h
+
+encodings: ; undocumented alternate encodings
+    mov eax, CONST
+    db 0f7h, 0c8h                           ; test eax, CONST
+        dd CONST
+    jz bad
+_
+    db 0f7h, 0c0h                           ; test eax, CONST
+        dd CONST
+    jz bad
+_
+    ; 'SAL' is technically the same as SHL, but different encoding
+    mov al, 1010b
+    db 0c0h, 0f0h, 2                        ; sal al, 2
+    expect al, 101000b
+_
+    retn
+_c
+
+smswtrick:
+    smsw ax
+    cmp ax, 03bh
+    jnz bad
+
+    fnop
+    smsw ax
+    cmp ax, 031h   ; 03bh if debugged or
+    jnz bad
+
+_1:
+    smsw ax
+    cmp ax, 031h
+    jz _1
+    retn
+
+gstrick:
+    ; anti stepping with thread switch GS clearing
+%macro mov_gs 1
+    mov ax, %1
+    mov gs, ax
+%endmacro
+
+%macro cmp_gs 1
+    mov ax, gs
+    cmp ax, %1
+%endmacro
+
+%macro gsloop 0
+    mov_gs 3
+%%_not:
+    cmp_gs 3
+    jz %%_not
+%endmacro
+
+    mov_gs 3
+    cmp_gs 3
+    jnz bad     ; gs should still be 3
+
+    ; behavior-based anti-emulator
+    gsloop      ; infinite loop if gs is not eventually reset
+
+    ; timing based anti-emulator
+;gsloop ; not needed, since we just switched in, because gs is 0
+    rdtsc
+    mov ebx, eax
+gsloop
+    rdtsc
+    sub eax, ebx
+    cmp eax, 1000h     ; 2 consecutives rdtsc take less than 70 ticks, we expect a much bigger value here.
+    jae GSgood
+    jmp bad
+_c
+GSgood:
+    retn
+_c
+
+antis:
+
+    call gstrick
+    call smswtrick
+_
+TF equ 0100h
+    ; checking if the Trap Flag is set via pushf (sahf doesn't save TF)
+    pushf
+    pop eax
+    and eax, TF
+    expect eax, 0
+_
+    ; the same, but 'pop ss' prevents the debugger to step on pushf
+    push ss
+    pop ss
+    pushf
+    pop eax
+    and eax, TF
+    jnz bad
+_
+    ; anti-debug: rdtsc as a timer check
+    rdtsc
+    mov ebx, eax
+    mov ecx, edx
+    rdtsc
+    cmp eax, ebx
+    jle bad
+    expect edx, ecx
+_
+    retn
+_c
+
+get_ips:
+    ; get ip call
+    call $ + 5
+after_call:
+    pop eax
+    expect eax, after_call
+_
+    ; get ip far call
+    call far 01bh: $ + 7
+after_far:
+    pop eax
+    expect eax, after_far
+    pop eax
+    expect eax, _CS
+_
+    ; get ip f?stenv
 _fpu:
     fnop
     fnstenv [fpuenv]              ; storing fpu environment
     mov eax,[fpuenv.DataPointer]  ; getting the EIP of last fpu operation
     expect eax, _fpu
-
-
-    jmp good
-_c
-
-;%IMPORT ntdll.dll!KiFastSystemCallRet
+    ; using the FPU will change internal flags such as cr0
+_
+    retn
 _c
 
 W7_tests:
@@ -621,8 +806,33 @@ _
 _
     str ax
     expect ax, 40h
-    jmp good
+    retn
 _c
+
+%define PREFIX_BRANCH_TAKEN db 3eh
+%define PREFIX_BRANCH_NOT_TAKEN db 2eh
+
+disassembly:
+    ; the following lines are just to test common mistakes in output
+    retn
+PREFIX_BRANCH_TAKEN
+    jz $ + 2
+PREFIX_BRANCH_NOT_TAKEN
+    jnz $ + 2
+    call word disassembly
+    db 66h
+    retn
+    str eax
+    str ax
+    str [eax]
+    smsw eax
+    smsw ax
+    PREFIX_OPERANDSIZE                      ; YASM doesn't support bswap <reg16>
+        bswap eax
+    bswap eax
+    db 0f1h
+_c
+
 xlattable:
 times 35 db 0
          db 75
