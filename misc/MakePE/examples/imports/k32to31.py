1b31f4391819916f58e6c8e1113ff9fb5f30c7f1
Ange Albertini <angea@users.noreply.github.com>
2011-06-29 19:22:26 +0000

    fixed code

--- misc/MakePE/examples/imports/k32to31.py
+++ misc/MakePE/examples/imports/k32to31.py
@@ -6,21 +6,20 @@ import sys
 
 for root, dirs, files in os.walk('.'):
     for file in files:
-    if file.startswith("kernel31.dll"):
-        continue
+        if file.startswith("kernel31.dll"):
+            continue
         fn = root + '\\' + file
         try:
-        with open(fn, "rb") as f:
-        r = f.read()
+            with open(fn, "rb") as f:
+                r = f.read()
             pe = pefile.PE(data=r)
         except pefile.PEFormatError,s:
             continue
 
-    r = r.replace("kernel32", "kernel31").replace("KERNEL32", "KERNEL31")
+        r = r.replace("kernel32", "kernel31").replace("KERNEL32", "KERNEL31")
+        
+        os.rename(fn, fn + ".bak32")
+        with open(fn, "wb") as f:
+            f.write(r)
 
-    os.rename(fn, fn + ".bak32")
-    with open(fn, "wb") as f:
-        f.write(r)
-    continue
-    
     # removed: pseudo-smart import patching, not working for on the fly loading.
