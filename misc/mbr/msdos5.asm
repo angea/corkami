b0cab38212a23137baad73d91c9c3d58e3fa3383
Ange Albertini <angea@users.noreply.github.com>
2011-07-05 20:32:10 +0000

    msdos5 now matches checksum

--- misc/mbr/msdos5.asm
+++ misc/mbr/msdos5.asm
@@ -1,3 +1,9 @@
+org 7c00h
+NEWBASE equ 600h
+MBRLEN equ 100h
+MARKER equ 0AA55h
+bits 16
+
 start:
     jmp short next
     nop
@@ -17,8 +23,7 @@ word_7C1A dw 2
 word_7C1C dw 0
 word_7C1E dw 0
 word_7C20 dw 0
-    db    0
-    db    0
+dw 0
 byte_7C24 db 0
 byte_7C25 db 0
     dw 0FC29h
@@ -26,13 +31,11 @@ byte_7C25 db 0
 aRNoName db 'R!NO NAME    '
 aFat12    db 'FAT12   '
 
-
 next:
-
     cli
-    xor ax, ax
+    xor ax, ax ;ORIGINAL 33 c0
     mov ss, ax
-    mov sp,  start
+    mov sp, start
     push ss
     pop es
 
@@ -40,7 +43,7 @@ loc_7C48:
     mov bx, 'x'
 
 loc_7C4B:
-    lds si, ss:[bx]
+    lds si, [ss:bx]
     push ds
 
 loc_7C4F:
@@ -57,50 +60,57 @@ loc_7C52:
     rep movsb
     push es
     pop ds
-    mov byte [di-2], 0Fh
-    mov cx, ds:word_7C18
-    mov [di-7], cl
-    mov [bx+2], ax
+    mov byte [di - 2], 0Fh
+    mov cx, [word_7C18]
+    mov [di - 7], cl
+    mov [bx + 2], ax
     mov word [bx],  next
     sti
     int 13h    ; reset disk system
-    jb short loc_7CED
-    xor ax, ax
-    cmp ds:word_7C13, ax
+	
+    jb short loc_7CED ; short
+
+    xor ax, ax	;ORIGINAL 33 c0
+    cmp [word_7C13], ax
     jz short loc_7C84
-    mov cx, ds:word_7C13
-    mov ds:word_7C20, cx
+
+    mov cx, [word_7C13]
+    mov [word_7C20], cx
 
 loc_7C84:
-    mov al, ds:byte_7C10
-    mul ds:word_7C16
-    add ax, ds:word_7C1C
-    adc dx, ds:word_7C1E
-    add ax, ds:word_7C0E
+    mov al, [byte_7C10]
+    mul word [word_7C16]
+    add ax, [word_7C1C]
+    adc dx, [word_7C1E]
+    add ax, [word_7C0E]
     adc dx, 0
-    mov word ds:loc_7C50, ax
-    mov word ds:loc_7C52, dx
-    mov word ds:loc_7C48+1, ax
-    mov word ds:loc_7C4B, dx
+    mov word [loc_7C50], ax
+    mov word [loc_7C52], dx
+    mov word [loc_7C48 + 1], ax
+    mov word [loc_7C4B], dx
     mov ax, 20h ; ' '
-    mul ds:word_7C11
-    mov bx, word ds:byte_7C0B
-    add ax, bx
+    mul word [word_7C11]
+    mov bx, word [byte_7C0B]
+    add ax, bx ;ORIGINAL 03 C3
     dec ax
     div bx
-    add word ds:loc_7C48+1, ax
-    adc word ds:loc_7C4B, 0
+    add word [loc_7C48 + 1], ax
+    adc word [loc_7C4B], 0
     mov bx, 500h
-    mov dx, word ds:loc_7C52
-    mov ax, word ds:loc_7C50
+    mov dx, word [loc_7C52]
+    mov ax, word [loc_7C50]
     call sub_7D60
+	
     jb short loc_7CED
+
     mov al, 1
     call sub_7D81
+
     jb short loc_7CED
-    mov di, bx
+
+    mov di, bx ;ORIGINAL 8b fb
     mov cx, 0Bh
-    mov si,  aIoSysmsdosSys ; "IO      SYSMSDOS SYS"
+    mov si,  aIoSysmsdosSys
     repe cmpsb
     jnz short loc_7CED
     lea di, [bx+20h]
@@ -109,17 +119,17 @@ loc_7C84:
     jz short loc_7D05
 
 loc_7CED:
-    mov si,  aNonSystemDiskO ; "\r\nNon-System disk or disk error\r\nReplac"...
+    mov si,  aNonSystemDiskO
     call sub_7D52
-    xor ax, ax
+    xor ax, ax ;ORIGINAL 33 c0
     int 16h    ; KEYBOARD - READ CHAR FROM BUFFER, WAIT IF EMPTY
          ; Return: AH = scan code, AL = character
+
     pop si
     pop ds
     pop word [si]
     pop word [si+2]
     int 19h    ; DISK BOOT
-         ; causes reboot of disk system
 
 loc_7D00:
     pop ax
@@ -132,11 +142,11 @@ loc_7D05:
     mov ax, [bx+1Ah]
     dec ax
     dec ax
-    mov bl, ds:byte_7C0D
-    xor bh, bh
+    mov bl, [byte_7C0D]
+    xor bh, bh ;ORIGINAL 32 ff
     mul bx
-    add ax, word ds:loc_7C48+1
-    adc dx, word ds:loc_7C4B
+    add ax, word [loc_7C48 + 1]
+    adc dx, word [loc_7C4B]
     mov bx, 700h
     mov cx, 3
 
@@ -145,28 +155,33 @@ loc_7D20:
     push dx
     push cx
     call sub_7D60
+	
     jb short loc_7D00
+
     mov al, 1
     call sub_7D81
+
     pop cx
     pop dx
     pop ax
     jb short loc_7CED
-    add ax, 1
+
+    add ax, 1 ;ORIGINAL 05 01 00
     adc dx, 0
-    add bx, word ds:byte_7C0B
+    add bx, word [byte_7C0B]
     loop loc_7D20
-    mov ch, ds:byte_7C15
-    mov dl, ds:byte_7C24
-    mov bx, word ds:loc_7C48+1
-    mov ax, word ds:loc_7C4B
-    jmp far ptr 70h:0
+
+    mov ch, [byte_7C15]
+    mov dl, [byte_7C24]
+    mov bx, word [loc_7C48 + 1]
+    mov ax, word [loc_7C4B]
+    jmp far 070h:0
 
 
 
-sub_7D52 proc near
+sub_7D52:
     lodsb
-    or al, al
+    or al, al ;ORIGINAL 0a c0
     jz short locret_7D80
     mov ah, 0Eh
     mov bx, 7
@@ -174,53 +189,47 @@ sub_7D52 proc near
          ; AL = character, BH = display page (alpha modes)
          ; BL = foreground color (graphics modes)
     jmp short sub_7D52
-sub_7D52 endp
 
-
-
-
-sub_7D60 proc near
-    cmp dx, ds:word_7C18
+sub_7D60:
+    cmp dx, [word_7C18]
     jnb short loc_7D7F
-    div ds:word_7C18
+	
+    div word [word_7C18]
     inc dl
-    mov byte ds:loc_7C4F, dl
-    xor dx, dx
-    div ds:word_7C1A
-    mov ds:byte_7C25, dl
-    mov word ds:loc_7C4B+2, ax
+    mov byte [loc_7C4F], dl
+    xor dx, dx ;ORIGINAL 33 D2
+    div word [word_7C1A]
+    mov [byte_7C25], dl
+    mov word [loc_7C4B + 2], ax
     clc
     retn
-
 
 loc_7D7F:
     stc
 
 locret_7D80:
     retn
-sub_7D60 endp
 
-
-
-
-sub_7D81 proc near
+sub_7D81:
     mov ah, 2
-    mov dx, word ds:loc_7C4B+2
+    mov dx, word [loc_7C4B + 2]
     mov cl, 6
     shl dh, cl
-    or dh, byte ds:loc_7C4F
-    mov cx, dx
-    xchg ch, cl
-    mov dl, ds:byte_7C24
-    mov dh, ds:byte_7C25
-    int 13h    ; read sectores
+    or dh, byte [loc_7C4F]
+    mov cx, dx ;ORIGINAL 8b ca
+    xchg ch, cl ;ORIGINAL 86 e9
+    mov dl, [byte_7C24]
+    mov dh, [byte_7C25]
+    int 13h    ; read sectors
     retn
-sub_7D81 endp
-
 
 aNonSystemDiskO db 0Dh,0Ah
     db 'Non-System disk or disk error',0Dh,0Ah
     db 'Replace and press any key when ready',0Dh,0Ah,0
-aIoSysmsdosSys db 'IO      SYSMSDOS   SYS',0
-    db    0
-    dw 0AA55h
+aIoSysmsdosSys:
+ db 'IO      SYS'
+ db 'MSDOS   SYS',0
+    db 0
+    dw MARKER
+
+;CHECKSUM d689a49825dc93eb6ea5ed6d4bc2ed90
