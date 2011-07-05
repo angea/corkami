b6911d3caa4075497b01550558d71c2967982ad5
Ange Albertini <angea@users.noreply.github.com>
2011-07-05 21:53:30 +0000

    now matches checksum

--- misc/mbr/standard.asm
+++ misc/mbr/standard.asm
@@ -1,9 +1,14 @@
+org 7c00h
+NEWBASE equ 600h
+MBRLEN equ 100h
+MARKER equ 0AA55h
+
 start:
     cli
-    xor ax, ax
+    xor ax, ax ;ORIGINAL 33 c0
     mov ss, ax
-    mov sp,  start
-    mov si, sp
+    mov sp, start
+    mov si, sp ;ORIGINAL 8B F4
     push ax
     pop es
     push ax
@@ -13,7 +18,7 @@ start:
     mov di, 600h
     mov cx, 100h
     repne movsw
-    jmp far ptr 0:61Dh
+    jmp far 0:61Dh
 
     mov si, 7dbeh - 7600h
     mov bl, 4
@@ -21,22 +26,26 @@ start:
 loc_7C22:
     cmp byte [si], 80h ; '€'
     jz short loc_7C35
+
     cmp byte [si], 0
     jnz short InvalidPartition
+
     add si, 10h
     dec bl
     jnz short loc_7C22
+
     int 18h
 
 loc_7C35:
     mov dx, [si]
     mov cx, [si+2]
-    mov bp, si
+    mov bp, si ;ORIGINAL 8B ee
 
 loc_7C3C:
     add si, 10h
     dec bl
     jz short loc_7C5D
+
     cmp byte [si], 0
     jz short loc_7C3C
 
@@ -47,6 +56,7 @@ nextchar:
     lodsb
     cmp al, 0
     jz short endloop
+
     push si
     mov bx, 7
     mov ah, 0Eh
@@ -56,11 +66,8 @@ nextchar:
     pop si
     jmp short nextchar
 
-
 endloop:
-         ; seg000:endloopj
     jmp short endloop
-
 
 loc_7C5D:
     mov di, 5
@@ -70,10 +77,12 @@ loc_7C60:
     mov ax, 201h
     push di
     int 13h    ; read sectores
+
     pop di
     jnb short missingos
-    xor ax, ax
+    xor ax, ax ;ORIGINAL 33 C0
     int 13h    ; reset disk system
+
     dec di
     jnz short loc_7C60
     mov si,  aErrorLoadingOp - 7600h ; "Error loading operating system"
@@ -85,14 +94,17 @@ missingos:
     mov di,  marker
     cmp word [di], 0AA55h
     jnz short nextchar
-    mov si, bp
-    jmp far ptr start
+    mov si, bp ;ORIGINAL 8B F5
+    jmp far 0:start
 
 aInvalidPartiti db 'Invalid partition table',0
 aErrorLoadingOp db 'Error loading operating system',0
 aMissingOperati db 'Missing operating system',0
-    db 0E3h dup(0)
-    db 80h, 2 dup(1), 0, 0Bh, 7Fh, 0BFh, 0FDh, 3Fh, 3 dup(0)
+    times 0E3h db 0
+    db 80h, 1, 1, 0, 0Bh, 7Fh, 0BFh, 0FDh, 3Fh, 0,0,0
     db 0C1h, 40h, 5Eh
-    db 31h dup(0)
+    times 31h db 0
+
 marker    dw 0AA55h
+
+;CHECKSUM 5d5cf631a79d9b4c792f3bb759ddf904
