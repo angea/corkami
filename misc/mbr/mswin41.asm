7f2d41cb4cc34246a622019646657cdf4546f809
Ange Albertini <angea@users.noreply.github.com>
2011-07-05 21:04:39 +0000

    mswin41 now matches checksum

--- misc/mbr/mswin41.asm
+++ misc/mbr/mswin41.asm
@@ -1,4 +1,10 @@
-    jmp short loc_7C3E
+org 7c00h
+NEWBASE equ 600h
+MBRLEN equ 100h
+MARKER equ 0AA55h
+
+start:
+	jmp short next
     nop
 
 aMswin4_1 db 'MSWIN4.1',0
@@ -28,8 +34,8 @@ dword_7C1C dd 0
 aLBootdiskFat12 db ')!l',15h,27h,'BOOTDISK   FAT12   '
 
 
-loc_7C3E:        
-    xor cx, cx
+next:        
+    xor cx, cx ;ORIGINAL 33 c9
     mov ss, cx
     mov sp, 7BFCh
     push ss
@@ -48,20 +54,23 @@ loc_7C3E:
     rep movsb
     push es
     pop ds
-    mov bp,  loc_7C00
+    mov bp,  start
     mov byte [di-2], 0Fh
     cmp [bp+24h], cl
     jge short loc_7C8D
-    mov ax, cx
+	
+    mov ax, cx ;ORIGINAL 8b c1
     cwd
     call sub_7DF1
+	
     sub bx, 3Ah ; ':'
-    mov eax, ds:dword_7C1C
+    mov eax, [dword_7C1C]
 
 loc_7C7A:        
     cmp eax, [bx]
     mov dl, [bx-4]
     jnz short loc_7C88
+	
     or dl, 2
     mov [bp+2], dl
 
@@ -70,8 +79,8 @@ loc_7C88:
     jnb short loc_7C7A
 
 loc_7C8D:        
-    xor cx, cx
-    inc ds:byte_7DD8
+    xor cx, cx ;ORIGINAL 33 c9
+    inc byte [byte_7DD8]
 
 loc_7C93:        
     mov al, [bp+10h]
@@ -80,7 +89,7 @@ loc_7C93:
     add ax, [bp+1Ch]
     adc dx, [bp+1Eh]
     add ax, [bp+0Eh]
-    adc dx, cx
+    adc dx, cx ;ORIGINAL 13 d1
     mov si, [bp+11h]
     pusha
     mov [bp-4], ax
@@ -88,7 +97,7 @@ loc_7C93:
     mov ax, 20h ; ' '
     mul si
     mov bx, [bp+0Bh]
-    add ax, bx
+    add ax, bx ;ORIGINAL 03 c3
     dec ax
     div bx
     add [bp-4], ax
@@ -98,65 +107,71 @@ loc_7C93:
 loc_7CC3:        
     mov di, 700h
     call sub_7DF1
+	
     jb short loc_7D09
 
 loc_7CCB:        
     cmp [di], ch
     jz short loc_7CE6
+	
     pusha
     mov cl, 0Bh
     mov si,  byte_7DD8
     repe cmpsb
     popa
     jz short loc_7D17
+	
     dec si
     jz short loc_7CE6
+	
     add di, 20h ; ' '
-    cmp di, bx
+    cmp di, bx ;ORIGINAL 3b fb
     jb short loc_7CCB
+	
     jmp short loc_7CC3
 
 
 loc_7CE6:        
-    dec ds:byte_7DD8
+    dec byte [byte_7DD8]
     jnp short loc_7C93
+	
     mov si,  word_7D7F
 
 loc_7CEF:        
     lodsb
     cbw
-    add si, ax
+    add si, ax ;ORIGINAL 03 F0
 
 loc_7CF3:        
     lodsb
     cbw
     inc ax
     jz short loc_7D04
+	
     dec ax
     jz short loc_7D0E
+	
     mov ah, 0Eh
     mov bx, 7
     int 10h    ; - VIDEO - WRITE CHARACTER AND ADVANCE CURSOR (TTY WRITE)
          ; AL = character, BH = display page (alpha modes)
          ; BL = foreground color (graphics modes)
+		 
     jmp short loc_7CF3
-
 
 loc_7D04:        
     mov si,  aInvalidSystemD ; "'\r\nInvalid system disk"
     jmp short loc_7CEF
 
-
 loc_7D09:        
     mov si, ( word_7D7F+1)
     jmp short loc_7CEF
-
 
 loc_7D0E:        
     int 16h    ; KEYBOARD -
     pop si
     pop ds
-    pop large dword ptr [si]
+    pop dword [si] ; large
     int 19h    ; DISK BOOT
          ; causes reboot of disk system
 
@@ -170,10 +185,10 @@ loc_7D17:
     adc dx, [bp-2]
     mov cl, 4
     call sub_7DF2
-    jb short loc_7D09
-    jmp far ptr 70h:200h
 
-; START OF FUNCTION CHUNK FOR sub_7DF2
+    jb short loc_7D09
+
+    jmp far 70h:200h
 
 loc_7D37:        
     push dx
@@ -184,34 +199,38 @@ loc_7D37:
     push 10h
     xchg ax, cx
     mov ax, [bp+18h]
-    mov ds:526h, al
+    mov [526h], al
     xchg ax, si
     xchg ax, dx
-    xor dx, dx
+    xor dx, dx ;ORIGINAL 33 d2
     div si
     xchg ax, cx
     div si
     inc dx
-    xchg cx, dx
+    xchg dx, cx
     div word [bp+1Ah]
-    mov dh, dl
-    mov ch, al
+    mov dh, dl ;ORIGINAL 8a f2
+    mov ch, al ;ORIGINAL 8a e8
     ror ah, 2
-    or cl, ah
+    or cl, ah ;ORIGINAL 0a CC
     mov ax, 201h
     cmp byte [bp+2], 0Eh
     jnz short loc_7D6B
+
     mov ah, 42h ; 'B'
-    mov si, sp
+    mov si, sp ;ORIGINAL 8b f4
 
 loc_7D6B:        
     mov dl, [bp+24h]
     int 13h    ; DISK -
+
     popa
     popa
     jb short locret_7D7E
+
     inc ax
     jnz short loc_7D78
+
     inc dx
 
 loc_7D78:        
@@ -221,10 +240,8 @@ loc_7D78:
 
 locret_7D7E:        
     retn
-; END OF FUNCTION CHUNK FOR sub_7DF2
 
 word_7D7F dw 1803h    
-         ; seg000:loc_7D09o
 byte_7D81 db 1     
 aInvalidSystemD db 27h,0Dh,0Ah    
     db 'Invalid system disk'
@@ -241,28 +258,19 @@ aOSysmsdosSys db 'O      SYSMSDOS   SYS'
     db    1
     db    0
 
-
-
-sub_7DF1 proc near    
+sub_7DF1:
     inc cx
-sub_7DF1 endp
 
-
-
-
-sub_7DF2 proc near    
-
-; FUNCTION CHUNK AT 7D37 SIZE 00000048 BYTES
-
+sub_7DF2:
     mov bx, 700h
 
 loc_7DF5:        
     pusha
-    push large 0
+    push dword 0
     jmp loc_7D37
-sub_7DF2 endp
 
+dw 0
 
-    db    0
-    db    0
-marker    dw 0AA55h
+marker    dw MARKER
+
+;CHECKSUM 6132527d7c41cf9dd31a5a23285b820b
