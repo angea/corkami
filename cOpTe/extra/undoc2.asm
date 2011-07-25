7536496a0881c3657c8cf97b00f4eff33b3b9702
Ange Albertini <angea@users.noreply.github.com>
2011-07-25 18:44:42 +0000

    better visual order

--- cOpTe/extra/undoc2.asm
+++ cOpTe/extra/undoc2.asm
@@ -1,37 +1,33 @@
 %include '../header.inc'
 
 %macro __ 0
-;	align 16, db 90h
+	align 8, db 90h
 %endmacro
-;%IMPORTS
 
 EntryPoint:
 	db 0f1h			; icebp
-__
 
 	db 0d6h			; setalc
-__
 
-	db 0c0h, 0f0h		; sal
-		db 90h
-__
+	db 66h
+	bswap eax
+
+;	db 0c0h, 0f0h		; sal
+
+;		db 90h
+
+;	smsw eax
 
 	db 0f7h, 0c8h		; test
 		dd 90909090h
 
-__
-
 	db 0fh, 1eh, 84h, 0c0h	; nop [eax+eax*8], eax
 		dd 90909090h
-__
-	db 0fh, 20h
-		db 90h
-__
-	smsw eax
-__
-	db 66h
-	bswap eax
 
+	db 0fh, 20h		; mov eax, cr2
+		db 90h
+
+;%IMPORTS
 
 SECTION0SIZE equ $ - Section0Start
 SIZEOFIMAGE equ $ - IMAGEBASE
