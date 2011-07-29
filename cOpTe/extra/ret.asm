668011f71cc134d4ce4333f505fa2c1066b2f5fd
Ange Albertini <angea@users.noreply.github.com>
2011-07-29 21:11:58 +0000

    setup moved to TLS whitespace cleaning

--- cOpTe/extra/ret.asm
+++ cOpTe/extra/ret.asm
@@ -1,20 +1,15 @@
 %include '../header.inc'
 
-%macro __ 0
-    align 8, db 90h
-%endmacro
-
-MEM_RESERVE               equ 2000h
-MEM_TOP_DOWN              equ 100000h
+MEM_RESERVE        equ 2000h
+MEM_TOP_DOWN       equ 100000h
+MB_ICONINFORMATION equ 040h
 
 EntryPoint:
-	call setup
-	push good
-	db 66h
-	retn
+    push good
+    db 66h
+    retn
 
-int3
-align 4, int3
+_c
 
 good:
     push MB_ICONINFORMATION ; UINT uType
@@ -25,20 +20,23 @@ good:
 
     push 0                  ; UINT uExitCode
     call ExitProcess
+_c
 
 ;%IMPORT user32.dll!MessageBoxA
 ;%IMPORT kernel32.dll!ExitProcess
+_c
 
 tada db "Tada!", 0
 helloworld db "Hello World!", 0
+_d
 
 ;%IMPORTS
 
 times 100  db 0
 
 setup:
-	pushad
-	pushf
+    pushad
+    pushf
     push PAGE_READWRITE     ; ULONG Protect
     push MEM_RESERVE|MEM_COMMIT|MEM_TOP_DOWN     ; ULONG AllocationType
     push zwsize             ; PSIZE_T RegionSize
@@ -47,36 +45,42 @@ setup:
     push -1                 ; HANDLE ProcessHandle
     call ZwAllocateVirtualMemory
 
-	mov edi, good
-	and edi, 0ffffh
-	mov esi, bad
-	mov ecx, badsize
-	rep movsb
-	
-	popf
-	popad
+    mov edi, good
+    and edi, 0ffffh
+    mov esi, bad
+    mov ecx, badsize
+    rep movsb
+    
+    popf
+    popad
     retn
 
-
 ;%IMPORT ntdll.dll!ZwAllocateVirtualMemory
-MB_ICONINFORMATION equ 040h
 
 zwsize dd 0ffffh
 lpBuffer3 dd 1
 
 bad:
-	add esp, 2
+    add esp, 2
     push MB_ICONINFORMATION ; UINT uType
-    push badstr               ; LPCTSTR lpCaption
-    push byeworld         ; LPCTSTR lpText
+    push badstr             ; LPCTSTR lpCaption
+    push byeworld           ; LPCTSTR lpText
     push 0                  ; HWND hWnd
-	call [__imp__MessageBoxA]
+    call [__imp__MessageBoxA]
 
     push 0                  ; UINT uExitCode
     call [__imp__ExitProcess]
 
 badstr db 'Fail !', 0
 byeworld db 'Bye World !',0
+
+Image_Tls_Directory32:
+    StartAddressOfRawData dd Characteristics
+    EndAddressOfRawData   dd Characteristics
+    AddressOfIndex        dd Characteristics
+    AddressOfCallBacks    dd SizeOfZeroFill
+    SizeOfZeroFill        dd setup
+    Characteristics       dd 0
 
 badsize EQU $ - bad
 
