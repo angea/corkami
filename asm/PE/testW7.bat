6cb444b06db09eacf0882bcdb0503e9ab90f2152
Ange Albertini <angea@users.noreply.github.com>
2011-09-29 15:38:06 +0000

    added tiny PE32+

--- asm/PE/testW7.bat
+++ asm/PE/testW7.bat
@@ -1,6 +1,7 @@
 @call test.bat
 @echo Low alignments:
 @tinyW7.exe
+@tinyW7_3264.exe
 @tinyW7x64.exe
 @maxsec_lowaligW7.exe
 @nosectionW7.exe
--- asm/PE/tinyW7x64.asm
+++ asm/PE/tinyW7x64.asm
@@ -1,6 +1,6 @@
-; a 268-byte PE (as small as possible), W7 x64 Compatible
+; a 268-byte PE 32+ (as small as possible), W7 x64 only
 
-; same as the 252 bytes PE for W7, but the limit is 268 on W7 x64
+; similar to the 268 byte tiny PE, but more collapsing is required as code is bigger
 
 ;Ange Albertini, BSD Licence, 2010-2011
 
@@ -18,59 +18,61 @@ align 4, db 0
 istruc IMAGE_NT_HEADERS
     at IMAGE_NT_HEADERS.Signature, db 'PE',0,0
 iend
+
 istruc IMAGE_FILE_HEADER
-    at IMAGE_FILE_HEADER.Machine,               dw IMAGE_FILE_MACHINE_I386
+    at IMAGE_FILE_HEADER.Machine,               dw IMAGE_FILE_MACHINE_AMD64
+    at IMAGE_FILE_HEADER.TimeDateStamp
+
+hnprintf:
+    dw 0
+    db 'printf', 0
+
     at IMAGE_FILE_HEADER.Characteristics,       dw IMAGE_FILE_EXECUTABLE_IMAGE | IMAGE_FILE_32BIT_MACHINE
 iend
 
-istruc IMAGE_OPTIONAL_HEADER32
-    at IMAGE_OPTIONAL_HEADER32.Magic,                     dw IMAGE_NT_OPTIONAL_HDR32_MAGIC
-    at IMAGE_OPTIONAL_HEADER32.AddressOfEntryPoint,       dd EntryPoint - IMAGEBASE
-    at IMAGE_OPTIONAL_HEADER32.ImageBase,                 dd IMAGEBASE
-    at IMAGE_OPTIONAL_HEADER32.SectionAlignment,          dd 4      ; also sets e_lfanew
-    at IMAGE_OPTIONAL_HEADER32.FileAlignment,             dd 4
-    at IMAGE_OPTIONAL_HEADER32.MajorSubsystemVersion,     dw 4
-    at IMAGE_OPTIONAL_HEADER32.SizeOfImage,               dd 40h    ; can't be smaller
-    at IMAGE_OPTIONAL_HEADER32.Subsystem,                 db IMAGE_SUBSYSTEM_WINDOWS_CUI
-    at IMAGE_OPTIONAL_HEADER32.NumberOfRvaAndSizes,       dd 2
+istruc IMAGE_OPTIONAL_HEADER64
+    at IMAGE_OPTIONAL_HEADER64.Magic,                     dw IMAGE_NT_OPTIONAL_HDR64_MAGIC
+
+msvcrt.dll  db 'msvcrt.dll', 0 ; dll extension not required
+
+    at IMAGE_OPTIONAL_HEADER64.AddressOfEntryPoint,       dd EntryPoint - IMAGEBASE
+    at IMAGE_OPTIONAL_HEADER64.ImageBase,                 dq IMAGEBASE
+    at IMAGE_OPTIONAL_HEADER64.SectionAlignment,          dd 4      ; also sets e_lfanew
+    at IMAGE_OPTIONAL_HEADER64.FileAlignment,             dd 4
+    at IMAGE_OPTIONAL_HEADER64.MajorSubsystemVersion,     dw 4
+    at IMAGE_OPTIONAL_HEADER64.SizeOfImage,               dd 40h    ; can't be smaller
+    at IMAGE_OPTIONAL_HEADER64.Subsystem,                 dw IMAGE_SUBSYSTEM_WINDOWS_CUI
+
+msvcrt.dll_iat:
+__imp__printf:
+    dq hnprintf - IMAGEBASE
+    dq 0
+
+    at IMAGE_OPTIONAL_HEADER64.NumberOfRvaAndSizes,       dd 2
 iend
 
 ; data directories
-dd 0, 0
-dd Import_Descriptor - IMAGEBASE
+dd 0, 0, Import_Descriptor - IMAGEBASE, 0
 
-bits 32
+bits 64
+
 EntryPoint:
-    push helloworld
+    sub rsp, 8 * 5
+    lea ecx, [message]
     call [__imp__printf]
-    add esp, 1 * 4
+    add rsp, 8 * 5
     retn
 
-helloworld db " * tiny 268 bytes PE (W7 x64)", 0ah, 0
+message db " * tiny 268 bytes PE32+ (W7 64b only)", 0ah, 0
 
 Import_Descriptor:
 ;msvcrt.dll_DESCRIPTOR
-    dd msvcrt.dll_hintnames - IMAGEBASE
+    dd msvcrt.dll_iat - IMAGEBASE
     dd 0, 0
     dd msvcrt.dll - IMAGEBASE
     dd msvcrt.dll_iat - IMAGEBASE
 ;terminator
     dd 0, 0, 0, 0, 0
 
-hnprintf:
-    dw 0
-    db 'printf', 0
-
-msvcrt.dll_iat:
-__imp__printf:
-    dd hnprintf - IMAGEBASE
-    dd 0
-
-msvcrt.dll_hintnames:
-    dd hnprintf - IMAGEBASE
-    dd 0
-
-msvcrt.dll  db 'msvcrt.dll'
-
-;filling up to 268 bytes for W7 x64, irritating... :(
-times 11 db 0
+;filling up to 268 bytes :(
+times 14 db 0
