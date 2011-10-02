cead9cf3d5bb7fac8262c8d46d03c6bab9584947
Ange Albertini <angea@users.noreply.github.com>
2011-10-02 21:30:38 +0000

    removed reimplemented duplicated

--- misc/MakePE/examples/PE/no_relocs_loader.asm
+++
@@ -1,9 +0,0 @@
-%include '..\..\standard_hdr.asm'
-
-EntryPoint EQU 330200h
-;%IMPORT no_relocs.dll!Export
-;%IMPORTS
-
-%include '..\..\standard_ftr.asm'
-
-; Ange Albertini, Creative Commons BY, 2009-2010
