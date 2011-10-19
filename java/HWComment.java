1af0648265e1400cb480a006a6cb65810636f1d4
Ange Albertini <angea@users.noreply.github.com>
2011-10-19 17:46:36 +0000

    Hello World comment source

---
+++ java/HWComment.java
@@ -0,0 +1,29 @@
+// compiled comment, as shown by Michael Shierl
+
+// javac HWComment.java
+// java HWComment
+
+// Ange Albertini, BSD Licence 2011
+
+public class HWComment
+{
+  public static void main(String[] args)
+  {
+  /*
+\u002a\u002f
+   \u0053\u0079\u0073\u0074\u0065\u006d\u002e
+   \u006f\u0075\u0074\u002e
+   \u0070\u0072\u0069\u006e\u0074\u006c\u006e
+   \u0028\u0022\u0048\u0065\u006c\u006c\u006f\u0020\u0057\u006f\u0072\u006c\u0064\u0021\u0022\u0029\u003b
+\u002f\u002a
+  */
+  
+// which is unicode encoding of:
+//  */ 
+//  System.
+//  out.
+//  println
+//  ("Hello World!");
+// /*
+  }
+}
