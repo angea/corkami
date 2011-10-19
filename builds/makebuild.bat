bf5a91f56f7bf71808388ee31c2e500af0e25718
Ange Albertini <angea@users.noreply.github.com>
2011-10-19 19:19:38 +0000

    builds-making dir and scripts

---
+++ builds/makebuild.bat
@@ -0,0 +1,14 @@
+@echo off
+del PE /q /s
+rd PE
+set today=%date:~-4%%date:~-10,2%%date:~-7,2%
+svn export http://corkami.googlecode.com/svn/trunk/asm/PE PE --force
+cd PE\bin
+call make
+:call test > test.txt
+:bin.sha
+cd ..
+"c:\program files\winrar\rar.exe" a ..\CPC-%today%-r.rar -r -m5 -s
+cd ..
+svn info http://corkami.googlecode.com/svn/trunk/asm/PE
+echo CPC-%today%-r
