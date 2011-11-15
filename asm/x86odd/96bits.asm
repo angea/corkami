1bd64a1214d791ad6a40e67b86a567d567504c90
Ange Albertini <angea@users.noreply.github.com>
2011-11-15 12:20:08 +0000

    minor

--- asm/x86odd/96bits.asm
+++ asm/x86odd/96bits.asm
@@ -57,8 +57,6 @@ iend
 NUMBEROFSECTIONS equ ($ - SectionHeader) / IMAGE_SECTION_HEADER_size
 SIZEOFHEADERS equ $ - IMAGEBASE
 
-db "this will not be in memory, as it's between the declared header and before the first section"
-
 section progbits vstart=IMAGEBASE + SECTIONALIGN align=FILEALIGN
 
 
