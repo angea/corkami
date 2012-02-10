8bd6bc7f60b5c2e52ed036721eafe10e7ecb8909
Ange Albertini <angea@users.noreply.github.com>
2012-02-10 22:07:57 +0000

    now genuine Java + javascript

---
+++ java/HW.java
@@ -0,0 +1,22 @@
+/*
+HelloWorld in Java and JavaScript
+ 
+thanks to the way Java unescapes unicode before comments
+
+Ange Albertini, BSD 2012
+*/
+
+/*
+\u002a\u002f
+
+public class HW
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World! [Java]");
+		String s = "*/alert('Hello World! [Javascript]');/*";
+	}
+}
+
+\u002f\u002a
+*/
