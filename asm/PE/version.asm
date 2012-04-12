3b2a6b176a3fed46ba739c8fe6bdbe4ea8c0531f
Ange Albertini <angea@users.noreply.github.com>
2012-04-12 19:14:21 +0000

    fixed

--- asm/PE/version.asm
+++ asm/PE/version.asm
@@ -1,4 +1,4 @@
-; a PE with version info (still broken)
+; a PE with version info
 
 ; Ange Albertini, BSD LICENCE 2012
 
@@ -63,7 +63,6 @@ iend
 OptionalHeader:
 istruc IMAGE_OPTIONAL_HEADER32
     at IMAGE_OPTIONAL_HEADER32.Magic,                     dw IMAGE_NT_OPTIONAL_HDR32_MAGIC
-    at IMAGE_OPTIONAL_HEADER32.AddressOfEntryPoint,       dd EntryPoint - IMAGEBASE
     at IMAGE_OPTIONAL_HEADER32.ImageBase,                 dd IMAGEBASE
     at IMAGE_OPTIONAL_HEADER32.SectionAlignment,          dd SECTIONALIGN
     at IMAGE_OPTIONAL_HEADER32.FileAlignment,             dd FILEALIGN
@@ -75,7 +74,6 @@ istruc IMAGE_OPTIONAL_HEADER32
 iend
 
 istruc IMAGE_DATA_DIRECTORY_16
-;    at IMAGE_DATA_DIRECTORY_16.ImportsVA,   dd Import_Descriptor - IMAGEBASE
     at IMAGE_DATA_DIRECTORY_16.ResourceVA, dd Directory_Entry_Resource - IMAGEBASE
     at IMAGE_DATA_DIRECTORY_16.ResourceSize, dd RESOURCE_SIZE
 	
@@ -97,62 +95,7 @@ SIZEOFHEADERS equ $ - IMAGEBASE
 
 section progbits vstart=IMAGEBASE + SECTIONALIGN align=FILEALIGN
 
-;;    push eax
-;;    call [__imp__printf]
-;;    add esp, 1 * 4
-;
-;    push 0
-;    call [__imp__ExitProcess]
-;_c
-;
-;Import_Descriptor:
-;;kernel32.dll_DESCRIPTOR:
-;    dd kernel32.dll_hintnames - IMAGEBASE
-;    dd 0, 0
-;    dd kernel32.dll - IMAGEBASE
-;    dd kernel32.dll_iat - IMAGEBASE
-;;msvcrt.dll_DESCRIPTOR:
-;    dd msvcrt.dll_hintnames - IMAGEBASE
-;    dd 0, 0
-;    dd msvcrt.dll - IMAGEBASE
-;    dd msvcrt.dll_iat - IMAGEBASE
-;;terminator
-;    dd 0, 0, 0, 0, 0
-;_d
-;
-;kernel32.dll_hintnames:
-;    dd hnExitProcess - IMAGEBASE
-;    dd 0
-;msvcrt.dll_hintnames:
-;    dd hnprintf - IMAGEBASE
-;    dd 0
-;_d
-;
-;hnExitProcess:
-;    dw 0
-;    db 'ExitProcess', 0
-;hnprintf:
-;    dw 0
-;    db 'printf', 0
-;_d
-;
-;kernel32.dll_iat:
-;__imp__ExitProcess:
-;    dd hnExitProcess - IMAGEBASE
-;    dd 0
-;
-;msvcrt.dll_iat:
-;__imp__printf:
-;    dd hnprintf - IMAGEBASE
-;    dd 0
-;_d
-;
-;kernel32.dll db 'kernel32.dll', 0
-;msvcrt.dll db 'msvcrt.dll', 0
-;_d
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
+; REQUIRED to start with the section !
 Directory_Entry_Resource:
 ; root directory
 istruc IMAGE_RESOURCE_DIRECTORY
@@ -197,7 +140,6 @@ VS_VERSION_INFO:
 	Value:
 		istruc VS_FIXEDFILEINFO
 			at VS_FIXEDFILEINFO.dwSignature, dd 0FEEF04BDh
-			dd 10000h
 		iend
 	VALUELENGTH equ $ - Value
 		align 4, db 0
@@ -213,24 +155,26 @@ VS_VERSION_INFO:
 			dw STRINGTABLELEN
 			dw 0 ; no value
 			dw 0
-			WIDE '040904b0'
+			WIDE '040904b0' ; required correct
 				align 4, db 0
 			;children
-			__string 'FileDescription', 'Test'
-
-;StringFileInfo
-;Comments
-;CompanyName
-;FileDescription
-;FileVersion
-;InternalName
-;LegalCopyright
-;LegalTrademarks
-;OriginalFilename
-;PrivateBuild
-;ProductName
-;ProductVersion
-;SpecialBuild
+			__string 'FileVersion', '' ; required or won't be displayed by explorer
+			__string 'FileDescription', 'a PE with version information'
+			__string 'LegalCopyright', ''
+			
+			__string 'custom', 'some value'
+				;other possible standard values
+				;StringFileInfo
+				;Comments
+				;CompanyName
+				;FileVersion
+				;InternalName
+				;LegalTrademarks
+				;OriginalFilename
+				;PrivateBuild
+				;ProductName
+				;ProductVersion
+				;SpecialBuild
 
 		STRINGTABLELEN equ $ - StringTable
 	STRINGFILEINFOLEN equ $ - StringFileInfo
@@ -243,23 +187,21 @@ VS_VERSION_INFO:
 			align 4, db 0
 		; children
 		Var1:
-			dw VAR1LEN
-			dw VAR1VALLEN
-			dw 0
-			WIDE 'Translation'
-				align 4, db 0
-			Var1Val:
-				dd 04b00409h
-			VAR1VALLEN equ $ - Var1Val
-				align 4, db 0
+		;	dw VAR1LEN
+		;	dw VAR1VALLEN
+		;	dw 0
+		;	WIDE 'Translation'
+		;		align 4, db 0
+		;	Var1Val:
+		;		dd 04b00h << 16 + 409h
+		;	VAR1VALLEN equ $ - Var1Val
+		;		align 4, db 0
 		VAR1LEN equ $ - Var1
 	VARFILEINFOLENGTH equ $ - VarFileInfo
 VERSIONLENGTH equ $ - VS_VERSION_INFO
 
 RESOURCE_SIZE equ $ - resource_data
 _d
-
-EntryPoint db 0c3h
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
