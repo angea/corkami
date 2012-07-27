9a9859dcd756fbaaa559440bdab51690d0292bfa
Ange Albertini <angea@users.noreply.github.com>
2012-07-27 16:05:23 +0000

    cleaned constant pool all fields documented

--- java/corkami.asm
+++ java/corkami.asm
@@ -4,17 +4,17 @@
 
 %macro GETSTATIC 1
         db 0b2h
-        _dw %1
+            _dw %1
 %endmacro
 
 %macro LDC 1
         db 012h
-        db %1
+            db %1
 %endmacro
 
 %macro INVOKEVIRTUAL 1
         db 0b6h
-        _dw %1
+            _dw %1
 %endmacro
 
 %macro RETURN 0
@@ -73,56 +73,63 @@ _dw %%end - 1 -$
 %endmacro
 
 _dd 0CAFEBABEh ; signature
-_dw  3         ; major version
+_dw 3          ; major version
 _dw 2dh        ; minor version
 
-_dw 018h       ;constant pool count
-        nat 14h, 17h                     ; 01
-        utf8 '([Ljava/lang/String;)V'    ; 02
-        utf8 'java/lang/Object'          ; 03
-        classref 3                       ; 04
-        utf8 'Hello World!'              ; 05
-        classref 10h                     ; 06
-        utf8 ''                          ; 07
-        utf8 'corkami'                   ; 08
-        string 5                         ; 09
-        classref 12h                     ; 0A
-        utf8 'Code'                      ; 0B
-        utf8 'main'                      ; 0C
-        fieldref 0ah, 1                  ; 0D
-        utf8 'SourceFile'                ; 0E
-        nat 11h, 15h                     ; 0F
-        utf8 'java/io/PrintStream'       ; 10
-        utf8 'println'                   ; 11
-        utf8 'java/lang/System'          ; 12
-        metref 6, 0fh                    ; 13
-        utf8 'out'                       ; 14
-        utf8 '(Ljava/lang/String;)V'     ; 15
-        classref 8                       ; 16
-        utf8 'Ljava/io/PrintStream;'     ; 17
-_dw 33  ;access_flag
+_dw 22        ;constant pool count
+  classref 2                           ; 01
+      utf8 'corkami'                   ; 02
 
-_dw 16h ;this_class
-_dw 4   ;superclass
+  classref 4                           ; 03
+      utf8 'java/lang/Object'          ; 04
 
-_dw 0   ;interfaces_count
-;fields_count
-_dw 0
-;methods_count
-_dw 1
-        _dw 9
-        _dw 0ch
-        _dw 2
-        _dw 1
-                _dw 0bh
-                _dd 15h
-                _dw 2
-                _dw 1
-                _dd 9
-                        GETSTATIC 0dh
-                        LDC 9
-                        INVOKEVIRTUAL 13h
-                        RETURN 
-                _dd 0
-;attributes_count 0
-_dw 0
+  utf8 'main'                          ; 05
+
+  utf8 'Code'                          ; 06
+
+  fieldref 8, 10                       ; 07
+      classref 9                       ; 08
+          utf8 'java/lang/System'      ; 09
+      nat 11, 12                       ; 10
+          utf8 'out'                   ; 11
+          utf8 'Ljava/io/PrintStream;' ; 12
+
+  string 14                            ; 13
+   utf8 'Hello World!'                 ; 14
+
+  metref 16, 18                        ; 15
+      classref 17                      ; 16
+          utf8 'java/io/PrintStream'   ; 17
+      nat 19, 20                       ; 18
+          utf8 'println'               ; 19
+          utf8 '(Ljava/lang/String;)V' ; 20
+
+  utf8 '([Ljava/lang/String;)V'        ; 21
+
+_dw 1  ;access_flag: public
+
+_dw 1 ;this class
+_dw 3 ;super class
+
+_dw 0 ; interfaces_count
+
+_dw 0 ; fields_count
+
+_dw 1 ; methods_count
+    _dw 9  ; flags: public, static
+    _dw 5  ; methodname: 'main'
+    _dw 21 ; return type: ([Ljava/lang/String;)V
+    _dw 1  ; attribute_count
+        _dw 6   ; attributename: Code
+        _dd 15h ; length
+            _dw 2 ; maxlocals
+            _dw 1 ; maxstack
+            _dd 9 ; length of bytecode
+                GETSTATIC 7
+                LDC 13
+                INVOKEVIRTUAL 15
+                RETURN
+            _dw 0 ; exceptions_count
+            _dw 0 ; attributes_count
+
+_dw 0 ;attributes_count
