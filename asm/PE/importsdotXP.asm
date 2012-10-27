0f2295568418b644665f3c930060df698d41bb15
Ange Albertini <angea@users.noreply.github.com>
2012-10-27 19:39:21 +0000

    imports with trailing dots dll-ld reverted to previous updated checksums, readme

--- asm/PE/importsdotXP.asm
+++ asm/PE/importsdotXP.asm
@@ -1,6 +1,6 @@
-; dll loader
+; a PE using trailing dots in its imports (XP only)
 
-; Ange Albertini, BSD LICENCE 2009-2011
+; Ange Albertini, BSD LICENCE 2012
 
 %include 'consts.inc'
 
@@ -34,15 +34,13 @@ istruc IMAGE_OPTIONAL_HEADER32
     at IMAGE_OPTIONAL_HEADER32.ImageBase,                 dd IMAGEBASE
     at IMAGE_OPTIONAL_HEADER32.SectionAlignment,          dd SECTIONALIGN
     at IMAGE_OPTIONAL_HEADER32.FileAlignment,             dd FILEALIGN
-    at IMAGE_OPTIONAL_HEADER32.MajorSubsystemVersion,     dw 3
-    at IMAGE_OPTIONAL_HEADER32.MinorSubsystemVersion,     dw 10
+    at IMAGE_OPTIONAL_HEADER32.MajorSubsystemVersion,     dw 4
     at IMAGE_OPTIONAL_HEADER32.SizeOfImage,               dd 2 * SECTIONALIGN
     at IMAGE_OPTIONAL_HEADER32.SizeOfHeaders,             dd SIZEOFHEADERS
     at IMAGE_OPTIONAL_HEADER32.Subsystem,                 dw IMAGE_SUBSYSTEM_WINDOWS_CUI
     at IMAGE_OPTIONAL_HEADER32.NumberOfRvaAndSizes,       dd 16
 iend
 
-DataDirectory:
 istruc IMAGE_DATA_DIRECTORY_16
     at IMAGE_DATA_DIRECTORY_16.ImportsVA,   dd Import_Descriptor - IMAGEBASE
 iend
@@ -57,28 +55,33 @@ istruc IMAGE_SECTION_HEADER
     at IMAGE_SECTION_HEADER.Characteristics,  dd IMAGE_SCN_MEM_EXECUTE | IMAGE_SCN_MEM_WRITE
 iend
 NUMBEROFSECTIONS equ ($ - SectionHeader) / IMAGE_SECTION_HEADER_size
-
 SIZEOFHEADERS equ $ - IMAGEBASE
 
 section progbits vstart=IMAGEBASE + SECTIONALIGN align=FILEALIGN
 
 EntryPoint:
-    call [__imp__export]
+    push Msg
+    call [__imp__printf]
+    add esp, 1 * 4
+_
     push 0
     call [__imp__ExitProcess]
 _c
 
+Msg db " * a PE using trailing dots in its imports (XP only)", 0ah, 0
+_d
+
 Import_Descriptor:
-kernel32.dll_DESCRIPTOR:
+;kernel32.dll_DESCRIPTOR:
     dd kernel32.dll_hintnames - IMAGEBASE
     dd 0, 0
     dd kernel32.dll - IMAGEBASE
     dd kernel32.dll_iat - IMAGEBASE
-dll.dll_DESCRIPTOR:
-    dd dll.dll_hintnames - IMAGEBASE
+;msvcrt.dll_DESCRIPTOR:
+    dd msvcrt.dll_hintnames - IMAGEBASE
     dd 0, 0
-    dd dll.dll - IMAGEBASE
-    dd dll.dll_iat - IMAGEBASE
+    dd msvcrt.dll - IMAGEBASE
+    dd msvcrt.dll_iat - IMAGEBASE
 ;terminator
     dd 0, 0, 0, 0, 0
 _d
@@ -86,38 +89,32 @@ _d
 kernel32.dll_hintnames:
     dd hnExitProcess - IMAGEBASE
     dd 0
-_d
-
-dll.dll_hintnames:
-    dd hndllexport - IMAGEBASE
+msvcrt.dll_hintnames:
+    dd hnprintf - IMAGEBASE
     dd 0
 _d
 
 hnExitProcess:
     dw 0
     db 'ExitProcess', 0
-_d
-
-hndllexport:
+hnprintf:
     dw 0
-    db 'export', 0
+    db 'printf', 0
 _d
 
 kernel32.dll_iat:
 __imp__ExitProcess:
     dd hnExitProcess - IMAGEBASE
     dd 0
-_d
-dll.dll_iat:
-__imp__export:
-    dd hndllexport - IMAGEBASE
+
+msvcrt.dll_iat:
+__imp__printf:
+    dd hnprintf - IMAGEBASE
     dd 0
 _d
 
-kernel32.dll db 'kernel32.dll', 0
-dll.dll db 'dll.dll', 0
+kernel32.dll db 'kernel32.dll...', 0
+msvcrt.dll db 'msvcrt.dll.', 0
 _d
 
 align FILEALIGN, db 0
-
-SIZEOFIMAGE EQU $ - IMAGEBASE
--- asm/PE/readme.txt
+++ asm/PE/readme.txt
@@ -1,14 +1,14 @@
-this is Corkami PE files corpus: 
+this is Corkami PE files corpus:
 a set of handmade files showing the various possibilities of the Portable Executable format,
-under Windows XP and 7.
+under Windows.
 
 All these files are clean and working.
 However, they are hand-made and push the PE file format to its limits,
 so they might be detected as malicious or as corrupted files.
 
-check bin\test*.txt for the descriptions of each binary
+it's documented at http://pe.corkami.com
 
 Ange Albertini
 @ange4771 (@corkami for news only)
 
-BSD Licence, 2009-2011
+BSD Licence, 2009-2012
