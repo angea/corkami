fd50e6e4db8fad12e865d8ccf2c4b188b6ece6f8
Ange Albertini <angea@users.noreply.github.com>
2013-03-31 10:18:46 +0000

    added conditional keypress, cleanup

--- misc/laby/laby.asm
+++ misc/laby/laby.asm
@@ -3,14 +3,22 @@
 
 ; thanks to herm1t, Solar Designer, qkumba, Rrrola
 
+%define RANDOM
+%define CLEANUP
+
 ; Ange Albertini BSD licence 2013
 
 bits 16
 
 org 100h
 
+SET_VIDEO_MODE equ 0
+MODE_T_640_400 equ 2
 MODE_320_200 equ 13h
 INT_VIDEO equ 10h
+
+READ_INPUT equ 0
+INT_KEYBOARD equ 16h
 
 PORT_TIMER equ 40h
 
@@ -94,22 +102,23 @@ pick_a_point:
     call random
 
     ; horizontal or vertical ?
-    ; default, vertical scan
+    ; by default: horizontal scan
     test al, 2h
     jnz V
 
     ; horizontal
     cwd ; risky?
     inc dx
-V:
 
+V:
     ; positive or negative progression ?
+    ; by default: negative
     test al, 4h
     jnz P
 
     neg dx ; negative
-P:
 
+P:
     ; dx now contains the increment for the target pixel to check
     add si, dx
     add si, dx
@@ -122,16 +131,30 @@ P:
     sub si, dx ; go back half way
     mov byte [bx + si], COLOR_WHITE
 
-; seed re-initialization (optional)
+%ifdef RANDOM
+    ; seed re-initialization (4 bytes)
     in ax, 40h
     xor bp, ax
+%endif
 
     loop pick_a_point
 
+
+%ifdef CLEANUP
+    ; wait for keypress (4 bytes)
+    xor ax, ax ; mov ah, READ_INPUT
+    int INT_KEYBOARD
+
+    ; graphical clean-up (5 bytes)
+    mov ax,  (SET_VIDEO_MODE << 8) + MODE_T_640_400
+    int INT_VIDEO
+%endif
+
 ; end - using the random return to end the program
+    ; ret
 
 random:
-    imul bp, 8405h
+    imul bp, 07bfbh
     inc bp
 
     mov ax, W - 1  ; 0..FFFF -> 0..W-2
