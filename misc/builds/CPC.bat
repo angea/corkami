d9061f730ab4046cbdc3c37d838c3b0472dc9733
Ange Albertini <angea@users.noreply.github.com>
2013-05-28 15:45:05 +0000

    updated paths

--- misc/builds/CPC.bat
+++ misc/builds/CPC.bat
@@ -2,7 +2,7 @@
 del PE /q /s
 rd PE
 set today=%date:~-4%%date:~-10,2%%date:~-7,2%
-svn export http://corkami.googlecode.com/svn/trunk/asm/PE PE --force
+svn export http://corkami.googlecode.com/svn/trunk/src/PE PE --force
 cd PE\bin
 call make
 :call test > test.txt
@@ -10,5 +10,5 @@ call make
 cd ..
 "c:\program files\winrar\rar.exe" a ..\CPC-%today%-r.rar -r -m5 -s
 cd ..
-svn info http://corkami.googlecode.com/svn/trunk/asm/PE
+svn info http://corkami.googlecode.com/svn/trunk/src/PE
 echo CPC-%today%-r
