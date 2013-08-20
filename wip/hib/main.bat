28f114eb44270dc73a5ac9d6e8c29f3ba26feb99
Ange Albertini <angea@users.noreply.github.com>
2013-08-20 07:57:24 +0000

    fixed timings removed joke

--- wip/hib/main.bat
+++ wip/hib/main.bat
@@ -1,4 +1,4 @@
-# http://www.youtube.com/watch?v=tmY4-RMB0YY
-call vid2mng cleese-creativity.mp4 cleese_joke 0:2:19  200 25
-call vid2mng cleese-creativity.mp4 cleese_nat  0:3:59  100 25
-call vid2mng cleese-creativity.mp4 cleese_gymt 0:22:10 175 25
+rem http://www.youtube.com/watch?v=tmY4-RMB0YY
+rem call vid2mng cleese-creativity.mp4 cleese_joke 0:2:19  200 25
+call vid2mng cleese-creativity.mp4 cleese_nat  0:04:00 100 25
+call vid2mng cleese-creativity.mp4 cleese_gymt 0:22:11 150 25
