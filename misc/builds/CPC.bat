84f8e72b5d36b574c866a48e90cd309ac65c841c
Ange Albertini <angea@users.noreply.github.com>
2013-09-03 18:39:59 +0000

    added Wiki page 'integration'

--- misc/builds/CPC.bat
+++ misc/builds/CPC.bat
@@ -3,6 +3,9 @@ del PE /q /s
 rd PE
 set today=%date:~-4%%date:~-10,2%%date:~-7,2%
 svn export http://corkami.googlecode.com/svn/trunk/src/PE PE --force
+svn export http://corkami.googlecode.com/svn/wiki/PE.wiki PE\PE.wiki
+wget "https://code.google.com/p/corkami/wiki/PE?show=content"
+move "PE@show=content" PE\PE.html
 svn info http://corkami.googlecode.com/svn/trunk/src/PE > PE\info
 cd PE\bin
 call make
