11fadaaa244196ff87f2c853041a83f04365cbad
Ange Albertini <angea@users.noreply.github.com>
2013-11-18 20:24:41 +0000

    cleaner structure segment now covering .rodata

--- wip/elf/simple.asm
+++ wip/elf/simple.asm
@@ -23,7 +23,7 @@ istruc Elf32_Ehdr
     at Elf32_Ehdr.e_type,      dw ET_EXEC
     at Elf32_Ehdr.e_machine,   dw EM_386
     at Elf32_Ehdr.e_version,   dd EV_CURRENT
-    at Elf32_Ehdr.e_entry,     dd main
+    at Elf32_Ehdr.e_entry,     dd entry
     at Elf32_Ehdr.e_phoff,     dd phdr - ehdr
     at Elf32_Ehdr.e_shoff,     dd shdr - ehdr
     at Elf32_Ehdr.e_ehsize,    dw Elf32_Ehdr_size
@@ -43,17 +43,21 @@ istruc Elf32_Phdr
     at Elf32_Phdr.p_type,   dd PT_LOAD
     at Elf32_Phdr.p_vaddr,  dd ELFBASE
     at Elf32_Phdr.p_paddr,  dd ELFBASE
-    at Elf32_Phdr.p_filesz, dd main - ehdr + MAIN_SIZE
-    at Elf32_Phdr.p_memsz,  dd main - ehdr + MAIN_SIZE
+    at Elf32_Phdr.p_filesz, dd segment_start - ehdr + SEGMENT_SIZE
+    at Elf32_Phdr.p_memsz,  dd segment_start - ehdr + SEGMENT_SIZE
     at Elf32_Phdr.p_flags,  dd PF_R + PF_X
 iend
 PHNUM equ ($ - phdr) / Elf32_Phdr_size
 
 align 16, db 0
+
+segment_start:
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; .text section (code)
 
-main:
+text:
+entry:
     mov ecx, msg
     mov edx, MSG_LEN
     mov ebx, STDOUT
@@ -67,7 +71,7 @@ main:
     mov eax, SC_EXIT
     int 80h
 
-MAIN_SIZE equ $ - main
+TEXT_SIZE equ $ - text
 
 align 16, db 0
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -82,6 +86,9 @@ msg:
 RODATA_SIZE equ $ - rodata
 
 align 16, db 0
+
+SEGMENT_SIZE equ $ - segment_start
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ; .shtstrtab section (section names)
 
@@ -108,9 +115,9 @@ istruc Elf32_Shdr
     at Elf32_Shdr.sh_name,      dd atext - names
     at Elf32_Shdr.sh_type,      dd SHT_PROGBITS
     at Elf32_Shdr.sh_flags,     dd SHF_ALLOC + SHF_EXECINSTR
-    at Elf32_Shdr.sh_addr,      dd main
-    at Elf32_Shdr.sh_offset,    dd main - ehdr
-    at Elf32_Shdr.sh_size,      dd MAIN_SIZE
+    at Elf32_Shdr.sh_addr,      dd text
+    at Elf32_Shdr.sh_offset,    dd text - ehdr
+    at Elf32_Shdr.sh_size,      dd TEXT_SIZE
 iend
 
 istruc Elf32_Shdr
