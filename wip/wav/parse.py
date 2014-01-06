090d0354972f7505d7656ee805a2ab260525b59f
Ange Albertini <angea@users.noreply.github.com>
2014-01-06 21:21:37 +0000

    added colors, fixed maths

--- wip/wav/parse.py
+++ wip/wav/parse.py
@@ -1,9 +1,12 @@
 #stupid reverse parser to check validity
+TONE = "\x80\xD9\xFF\xD9\x80\x26\x01\x26" * 80
+SILENCE = "\x80\x80\x80\x80\x80\x80\x80\x80" * 80
 
 fn = 'simple.wav'
 with open(fn, 'rb') as f:
 	r = f.read()
-r = r[0x2c:]
-r = r.replace("\x80\xD9\xFF\xD9\x80\x26\x01\x26" * 80, "\xdb")
-r = r.replace("\x80\x80\x80\x80\x80\x80\x80\x80" * 80, " ")
-print r
+s = r[0x2c:]
+s = s.replace(TONE, "\xdb")
+s = s.replace(SILENCE, " ")
+print s
+
