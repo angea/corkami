c6f58d3e871c1f283e11b3fafc7c18d26a4e4f24
Ange Albertini <angea@users.noreply.github.com>
2014-01-08 20:11:39 +0000

    added hex output

--- wip/wav/parse.py
+++ wip/wav/parse.py
@@ -6,7 +6,19 @@ fn = 'simple.wav'
 with open(fn, 'rb') as f:
 	r = f.read()
 s = r[0x2c:]
+
+#print tone sequences
 s = s.replace(TONE, "\xdb")
 s = s.replace(SILENCE, " ")
 print s
+
+
+#print hex data
+l = []
+for c in s:
+    if c == " ":
+        l += ["  ".join([" ".join("%02X" % ord(i) for i in list(SILENCE)), "".join([80 * "........"])])]
+    else:
+        l += ["  ".join([" ".join("%02X" % ord(i) for i in list(TONE)), "".join([80 * ".....&.&"])])]
+print "\n".join(l)
 
